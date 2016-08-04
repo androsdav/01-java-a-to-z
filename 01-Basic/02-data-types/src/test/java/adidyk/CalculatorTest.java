9aa67c5538842edf387604dfcfb1195ab6b8d3cf
androsdav <androsdav@gmail.com>
2016-08-04 17:26:43 +0300

    add test

---
+++ 01-Basic/02-data-types/src/test/java/adidyk/CalculatorTest.java
@@ -0,0 +1,41 @@
+package adidyk;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+       @Test
+       public void testAdd() {
+              final Calculator calc = new Calculator();
+              calc.add(1,5);
+              final double result = calc.getResult();
+              assertThat(result, is(6d));
+       }
+
+       @Test
+       public void testSub() {
+              final Calculator calc = new Calculator();
+              calc.sub(10,3);
+              final double result = calc.getResult();
+              assertThat(result, is(7d));
+       }
+
+       @Test
+       public void testDiv() {
+              final Calculator calc = new Calculator();
+              calc.div(6,2);
+              double result = calc.getResult();
+              assertThat(result, is(3d));
+       }
+
+       @Test
+       public void testMult() {
+              final Calculator calc = new Calculator();
+              calc.mult(3,4);
+              final double result = calc.getResult();
+              assertThat(result, is(12d));
+       }
+}
