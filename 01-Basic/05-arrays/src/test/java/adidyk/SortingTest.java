113855b8cb36e56012e44b43324adcfec714cdb5
androsdav <androsdav@gmail.com>
2016-08-08 15:26:38 +0300

    add test

---
+++ 01-Basic/05-arrays/src/test/java/adidyk/SortingTest.java
@@ -0,0 +1,21 @@
+package adidyk;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+//import java.util.ArrayList;
+
+public class SortingTest {
+
+	@Test
+	public void babbleSortingTest() {
+		// Expected ArrayList
+		int []bubbleExpected = {1, 2, 3, 4, 5};
+		// Actual ArrayList	
+		int []bubbleActual = {4, 5, 2, 3, 1};
+		// Testing sort babble 				
+		final Sorting sort = new Sorting(bubbleActual);
+		bubbleActual = sort.babbleSorting();
+		assertThat(bubbleActual, is(bubbleExpected));
+	}
+}
