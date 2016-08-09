615a69f1008e067e90fc6996e065bc7fa341ba22
androsdav <androsdav@gmail.com>
2016-08-09 10:49:47 +0300

    add Square

--- 01-Basic/04-loops/src/main/java/adidyk/Square.java
+++ 01-Basic/04-loops/src/main/java/adidyk/Square.java
@@ -3,19 +3,15 @@ package adidyk;
 import java.util.ArrayList;
 
 public class Square {
-	float paramA;
-	float paramB;
-	float paramC;
-	float startX1;
-	float finishX2;
-		
+	final float paramA;
+	final float paramB;
+	final float paramC;
+			
 	//Constructor 
-	public Square(float paramA, float paramB, float paramC, float startX1, float finishX2) {
+	public Square(float paramA, float paramB, float paramC) {
 		this.paramA = paramA;
 		this.paramB = paramB;
 		this.paramC = paramC;
-		this.startX1 = startX1;
-		this.finishX2 = finishX2;
 	}
 
 	// calculate function on the point rootX
@@ -23,8 +19,8 @@ public class Square {
 		return (this.paramA * rootX * rootX + this.paramB * rootX + this.paramC);
 	}
 	// calculate function on the interval x1 to x2, with a step
-	public ArrayList<Float> getYfuncArray(float step, ArrayList<Float> functionY) {
-		for (float i = this.startX1; i < this.finishX2; i = i + step) {
+	public ArrayList<Float> range(float step, float startX1, float finishX2, ArrayList<Float> functionY) {
+		for (float i = startX1; i < finishX2; i = i + step) {
 			functionY.add(calculate(i));
 		}
 		return functionY;
