5057ab468f76271904eaff96292202f1b1855205
androsdav <androsdav@gmail.com>
2016-08-11 23:26:18 +0300

    add Dublicate

--- 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
+++ 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
@@ -1,36 +1,40 @@
 package adidyk;
 
 public class Dublicate {
-	final String []args;
-	final int []position;
-	int unit = 0;
-	
+	private String []args;
+		
 	// Constructor
 	public Dublicate(String []args) {
 		this.args = args;
-		this.position = new int[args.length];
 	}
-	// method searchDubl - searching dublicate  
-	public String []  searchDubl() {
-		//int unit = 0;
+	
+	// method searchDubl - searching dublicate in String Array and
+	// return length new Array don`t have dublicate
+	public int searchDublArray() {
+		int unitDubl = 0;
 		for (int i = 0; i < this.args.length - 1; i++) {
 			for (int j = i + 1; j < this.args.length; j++) {
 				if (this.args[i].equals(this.args[j])) {
-					this.args[i] = "false";
-				} else {
-					//this.position[k] = i;
-					this.unit = this.unit + 1;
-				}
-				
-				//	this.position[i] =
-					//sum = sum + 1;
+					this.args[i] = "----";
+					unitDubl = unitDubl + 1;
 				}
 			}
-		return this.args;	
+		}
+		return (this.args.length - unitDubl);	
 	}
 	
-	public int getSize() {
-		return this.unit;
+	// method createNewArray - Creating new array and don`t have dublicate 
+	public String [] createNewArray() {
+	String []argsNew = new String[searchDublArray()];
+		int j = 0;
+		for (int i = 0; i < this.args.length; i++) {
+			if (this.args[i].equals("----")) {
+			} else {
+				argsNew[j] = this.args[i];
+				j = j + 1;
+			}		
+		}
+		return argsNew;
 	}
 }
 
