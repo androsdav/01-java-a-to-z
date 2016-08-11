699706301f144317c6bf1e0f5f0ef8ad7f9140b2
androsdav <androsdav@gmail.com>
2016-08-11 17:52:20 +0300

    add test

--- 01-Basic/05-arrays/src/test/java/adidyk/DublicateTest.java
+++ 01-Basic/05-arrays/src/test/java/adidyk/DublicateTest.java
@@ -10,12 +10,16 @@ public class DublicateTest {
 	@Test
 	public void searcDubl() {
 		// Expected ArrayList
-		String []argsExpected = {"horse", "dog", "pig", "elephant", "cat", "false", "false", "false", "false", "mouse"};
+		String []argsExpected = {"horse", "false", "pig", "false", "false", "false", "dog", "cat", "elephant", "mouse"};
 		// Actual ArrayList	
 		String []argsActual = {"horse", "dog", "pig", "elephant", "cat", "dog", "dog", "cat", "elephant", "mouse"};
 		// Testing sort babble 				
 		final Dublicate dubl = new Dublicate(argsActual);
 		argsActual = dubl.searchDubl();
 		assertThat(argsActual, is(argsExpected));
+		
+		int size = dubl.getSize();
+		assertThat(size, is(5));
+		
 	}
-}
+} 
