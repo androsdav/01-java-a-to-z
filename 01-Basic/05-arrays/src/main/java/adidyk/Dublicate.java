dc5c8845b6d0d6cf6ab3235ed569933fe7decd98
androsdav <androsdav@gmail.com>
2016-08-11 17:49:01 +0300

    add Dubliate

--- 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
+++ 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
@@ -2,60 +2,37 @@ package adidyk;
 
 public class Dublicate {
 	final String []args;
-	final int []position; 
+	final int []position;
+	int unit = 0;
 	
 	// Constructor
 	public Dublicate(String []args) {
 		this.args = args;
 		this.position = new int[args.length];
-		
 	}
 	// method searchDubl - searching dublicate  
 	public String []  searchDubl() {
-		
+		//int unit = 0;
 		for (int i = 0; i < this.args.length - 1; i++) {
 			for (int j = i + 1; j < this.args.length; j++) {
 				if (this.args[i].equals(this.args[j])) {
-					this.args[j] = "false";
+					this.args[i] = "false";
+				} else {
+					//this.position[k] = i;
+					this.unit = this.unit + 1;
+				}
+				
 				//	this.position[i] =
 					//sum = sum + 1;
 				}
 			}
-		}
-		return this.args;
+		return this.args;	
 	}
-
+	
+	public int getSize() {
+		return this.unit;
+	}
 }
 
 
-
-
-
-	/*int []bubble;
-	
-	//Constructor
-	public Sorting(int []bubble) {
-		this.bubble = bubble;
-	}
-	
-	// Method babbleSort - sorting babble Array
-	/*public int [] babbleSorting() {
-		boolean flag = true;
-		int min;
-		while (flag) {
-			for (int j = bubble.length - 1; j > 0; j--) {
-				for (int i = 0; i < j; i++) {
-					if (this.bubble[i] > this.bubble[i + 1]) {
-						min = this.bubble[i + 1];
-						this.bubble[i + 1] = this.bubble[i];
-						this.bubble[i] = min;
-					}	
-					else {
-						flag = false;
-					}
-				}
-			}
-			flag = false;
-		}
-		return this.bubble;
-	}*/
+                                                                 
