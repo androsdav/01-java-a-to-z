e9da6bbe6ad71b107d0902329e5d72c75ae037b4
androsdav <androsdav@gmail.com>
2016-08-16 23:47:24 +0300

    add Dublicate

--- 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
+++ 01-Basic/05-arrays/src/main/java/adidyk/Dublicate.java
@@ -10,20 +10,21 @@ public class Dublicate {
 	
 	// Searching dublicate, move left all array on one position - deleting dublicate,
 	// and last element of array assigning "null", so, and return length new Array don`t have dublicate
-	public Integer searchDublArray() {
-		Integer shift = 0;
-		for (int i = 0; i < this.args.length - 1; i++) {	
-			for (int j = i + 1; j < this.args.length; j++) {
-				if ((this.args[i].equals(this.args[j])) & (!this.args[i].equals("null"))) {
-					for (int k = i; k < this.args.length - 1; k++) {
+	public int searchDublArray() {
+		int shift = 0;
+		int length = this.args.length - 1;
+		//Object o = null;
+		for (int i = 0; i < length - shift; i++) {	
+			for (int j = i + 1; j < this.args.length - shift; j++) {
+				if (this.args[i].equals(this.args[j])) {
+					for (int k = i; k < length - shift; k++) {
 						this.args[k] = this.args[k + 1];
 					}
-					this.args[this.args.length - 1 - shift] = null;
+					this.args[length - shift] = null;
 					shift = shift + 1;
 					j = i;
 				}
 			}
-			
 		}
 		return this.args.length - shift;
 	}
