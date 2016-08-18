0a0710bc865d4389183aeb87b865a1fd41ae0792
androsdav <androsdav@gmail.com>
2016-08-18 23:20:50 +0300

    add Substring

---
+++ 01-Basic/06-test/src/main/java/adidyk/Substring.java
@@ -0,0 +1,26 @@
+package adidyk;
+
+public class Substring {
+		
+		// Splitting origin string and substring, search substring in origin
+		// string: return "true" if hi`s, return "false" if hi isn`t 
+		public boolean subSearch(String origin, String sub) {
+			boolean validSub = false;
+			char []originArray = origin.toCharArray();
+			char []subArray = sub.toCharArray();
+			int sumSub = 0;
+			// search subArray in originArray
+			for (int i = 0; i <= originArray.length - subArray.length; i++) {
+				for (int j = 0; j < subArray.length; j++) {
+					if (subArray[j] == originArray[i + j]) {
+						sumSub = sumSub + 1;
+					}
+				}
+				if (sumSub == subArray.length) {
+					validSub = true;
+				}
+				sumSub = 0;
+			}
+			return validSub;
+		} 
+}
