a5b2e1f968af8af69a47eae96f4debcadb55e1bf
androsdav <androsdav@gmail.com>
2016-08-18 23:22:15 +0300

    add test

--- 01-Basic/06-test/src/test/java/adidyk/SubstringTest.java
+++ 01-Basic/06-test/src/test/java/adidyk/SubstringTest.java
@@ -4,20 +4,26 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-public class ContainsTest {
-	// Expected New String List
+public class SubstringTest {
+	
+	// Original string
 	String originActual = "Teacher open the door, but you must enter by yourself.";
-	// Expected String List
+	// Substring for search in the original string
+	String subActual = " the door, but you mu";
+	
+	// Array char split of origin string
 	char []originExpected = { 
 		'T', 'e', 'a', 'c', 'h', 'e', 'r', ' ', 'o', 'p', 'e', 'n', ' ', 't', 
 		'h', 'e', ' ', 'd', 'o', 'o', 'r', ',', ' ', 'b', 'u', 't', ' ', 'y',
 		'o', 'u', ' ', 'm', 'u', 's', 't', ' ', 'e', 'n', 't', 'e', 'r', ' ',
 		'b', 'y', ' ', 'y', 'o', 'u', 'r', 's', 'e', 'l', 'f', '.'};
+	// Array char split of subdtring
+	char []subExpected = {'o', 'p', 'e', 'n'};
 	
 	@Test
 	public void splittingTest() {
-		Contains cont = new Contains();
-		char []origin = cont.splitting(originActual);
-		assertThat(origin, is(originExpected));
+		Substring split = new Substring();	
+		boolean valid = split.subSearch(originActual, subActual);
+		assertTrue(valid);
 	}
 }
