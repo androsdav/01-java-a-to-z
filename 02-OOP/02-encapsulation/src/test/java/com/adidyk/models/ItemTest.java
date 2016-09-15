ac1fea4a19a60309ea156d818a9eb13f5198c208
androsdav <androsdav@gmail.com>
2016-09-15 12:03:33 +0300

    add test

---
+++ 02-OOP/02-encapsulation/src/test/java/com/adidyk/models/ItemTest.java
@@ -0,0 +1,49 @@
+package com.adidyk.models;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    Item item = new Item("task", "I wont to buy your product", 100);
+
+    @Test
+    public void setNameTest() {
+        this.item.setName("bag");
+        String result = this.item.getName();
+        assertThat(result, is("bag"));
+    }
+
+    @Test
+    public void setDescriptionTest() {
+        this.item.setDescription("Your product doesn't work");
+        String result = this.item.getDescription();
+        assertThat(result, is("Your product doesn't work"));
+    }
+
+    @Test
+    public void setCreateTest() {
+        this.item.setCreate(200);
+        long result = this.item.getCreate();
+        assertThat(result, is(200l));
+    }
+
+    @Test
+    public void getNameTest() {
+        String result = this.item.getName();
+        assertThat(result, is("task"));
+    }
+
+    @Test
+    public void getDescriptionTest() {
+        String result = this.item.getDescription();
+        assertThat(result, is("I wont to buy your product"));
+    }
+
+    @Test
+    public void getCreateTest() {
+        long result = this.item.getCreate();
+        assertThat(result, is(100l));
+    }
+}
