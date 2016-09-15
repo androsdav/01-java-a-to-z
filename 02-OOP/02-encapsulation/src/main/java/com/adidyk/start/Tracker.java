33e7274c318294581e338f924ba912fd6d409265
androsdav <androsdav@gmail.com>
2016-09-15 17:49:23 +0300

    add Tracker

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
@@ -3,8 +3,21 @@ import com.adidyk.models.Item;
 
 public class Tracker {
 
+    public Item[] item = new Item[1];
+    public int position = 0;
+
     public static void main(String[] arg) {
-        Item item = new Item("Andrey", "proverka text", 01);
-        System.out.println(item.getName() +" " +item.getDescription() +" " +item.getCreate());
+        //Item item = new Item("Andrey", "proverka text", 01);
+        //System.out.println(item.getName() +" " +item.getDescription() +" " +item.getCreate());
+        System.out.println("Hello world");
+    }
+
+    public Item addItem(Item item) {
+        this.item[this.position] = item;
+        return item;
+    }
+
+    public Item[] getItem() {
+        return this.item;
     }
 }
