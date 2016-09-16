c969ebbbb9d4e47ac03545e74263afeeae37cd15
androsdav <androsdav@gmail.com>
2016-09-16 17:53:16 +0300

    tracker

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
@@ -13,11 +13,19 @@ public class Tracker {
     }
 
     public Item addItem(Item item) {
-        this.item[this.position] = item;
+        if (this.position == this.item.length)
+        {
+            Item[] itemTemp = new Item[2 * this.item.length];
+            System.arraycopy(this.item, 0, itemTemp, 0, this.item.length);
+            this.item = itemTemp;
+        }
+        this.item[this.position++] = item;
         return item;
     }
 
-    public Item[] getItem() {
-        return this.item;
+    public Item[] getAllItem() {
+        Item[] result = new Item[this.position];
+        System.arraycopy(this.item, 0, result, 0, this.position);
+        return result;
     }
 }
