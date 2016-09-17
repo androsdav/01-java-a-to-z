76ee2e66256e5b460eeb4d47d6eb6ae4bd2ad365
androsdav <androsdav@gmail.com>
2016-09-18 01:35:26 +0300

    add Tracker

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
@@ -1,10 +1,12 @@
 package com.adidyk.start;
 import com.adidyk.models.Item;
+import java.util.*;
 
 public class Tracker {
 
     public Item[] item = new Item[1];
     public int position = 0;
+    public static final Random RN = new Random();
 
     public static void main(String[] arg) {
         //Item item = new Item("Andrey", "proverka text", 01);
@@ -19,6 +21,7 @@ public class Tracker {
             System.arraycopy(this.item, 0, itemTemp, 0, this.item.length);
             this.item = itemTemp;
         }
+        item.setId(this.generateId());
         this.item[this.position++] = item;
         return item;
     }
@@ -28,4 +31,20 @@ public class Tracker {
         System.arraycopy(this.item, 0, result, 0, this.position);
         return result;
     }
+    
+    public Item searchItemById(String id) {
+    	Item result = null;
+		for (Item item : this.item) {
+			if (item != null && item.getId().equals(id)) {
+				result = item;
+				break; 
+			}
+		}
+    	return result;
+    }
+    
+    public String generateId() {
+    	return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+    
 }
