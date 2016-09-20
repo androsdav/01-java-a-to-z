72385703d4030d704f465bc4cf619c2f179a59eb
androsdav <androsdav@gmail.com>
2016-09-20 17:52:44 +0300

    add test

--- 02-OOP/02-encapsulation/src/test/java/com/adidyk/start/TrackerTest.java
+++ 02-OOP/02-encapsulation/src/test/java/com/adidyk/start/TrackerTest.java
@@ -92,7 +92,7 @@ public class TrackerTest {
     }
            
     @Test
-    public void deleteItemIdTest() {
+    public void deleteItemByIdTest() {
     	Item item1 = new Item("task1", "description1", 1);
     	Item item2 = new Item("task2", "description2", 2);
     	Item item3 = new Item("task3", "description3", 3);
@@ -103,9 +103,65 @@ public class TrackerTest {
     	track.addItem(item2);
     	track.addItem(item3);
     	track.addItem(item4);
-    	track.deleteItemId(item3.getId());
+    	track.deleteItemById(item3.getId());
     	Item[] result = track.getAllItem();
     	assertThat(items, is(result));
     }
+
+	@Test
+	public void renameItemByNameTest() {
+		Item item1 = new Item("taskOld", "description", 1);
+		Item item2 = new Item("task2", "description", 2);
+		Item item3 = new Item("task3", "description", 3);
+		Item item4 = new Item("taskOld", "description", 4);
+		Item[] items = {item1, item2, item3, item4};
+		Tracker track =new Tracker();
+		track.addItem(item1);
+		track.addItem(item2);
+		track.addItem(item3);
+		track.addItem(item4);
+		track.searchItemByName("taskOld");
+		track.renameItemByName("taskNew");
+		Item[] result = track.getAllItem();
+		assertThat(items, is(result));
+	}
+
+	@Test
+	public void renameItemByDescriptionTest() {
+		Item item1 = new Item("task1", "descriptionOld", 1);
+		Item item2 = new Item("task2", "description", 2);
+		Item item3 = new Item("task3", "descriptionOld", 3);
+		Item item4 = new Item("task4", "descriptionOld", 4);
+		Item[] items = {item1, item2, item3, item4};
+		Tracker track =new Tracker();
+		track.addItem(item1);
+		track.addItem(item2);
+		track.addItem(item3);
+		track.addItem(item4);
+		track.searchItemByDescription("descriptionOld");
+		track.renameItemByDescription("descriptionNew");
+		Item[] result = track.getAllItem();
+		assertThat(items, is(result));
+	}
+
+	@Test
+	public void addCommentByIdTest() {
+		Item item1 = new Item("task", "description", 1);
+		Item item2 = new Item("task", "description", 2);
+		Item item3 = new Item("task", "description", 3);
+		Item item4 = new Item("task", "description", 4);
+		Item[] items = {item1, item2, item3, item4};
+		Tracker track = new Tracker();
+		track.addItem(item1);
+		track.addItem(item2);
+		track.addItem(item3);
+		track.addItem(item4);
+		track.searchItemById(item3.getId());
+		track.addCommentById("comment1");
+		track.addCommentById("comment2");
+		track.addCommentById("comment3");
+		Item[] result = track.getAllItem();
+		assertThat(items, is(result));
+	}
 
 }
