45786b9a738863d0293f6fbe2e395a1f51ca5ee6
androsdav <androsdav@gmail.com>
2016-09-20 17:50:50 +0300

    add Item

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/models/Item.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/models/Item.java
@@ -6,6 +6,8 @@ public class Item {
     public String description;
     public long create;
     private String id;
+    public String[] comment =new String[1];
+    private int position = 0;
 
     public Item(String name, String description, long create) {
         this.name = name;
@@ -13,35 +15,53 @@ public class Item {
         this.create = create;
     }
 
-        public void setName(String name) {
+    public void setName(String name) {
             this.name = name;
         }
 
-        public void setDescription(String description) {
-            this.description = description;
+    public void setDescription(String description) {
+        this.description = description;
         }
 
-        public void setCreate(long create) {
-           this.create = create;
+    public void setCreate(long create) {
+        this.create = create;
         }
         
-        public void setId(String id) {
-        	this.id = id;
+    public void setId(String id) {
+        this.id = id;
         }
 
-        public String getName() {
-            return this.name;
+    public String getDescription() {
+        return this.description;
         }
 
-        public String getDescription() {
-            return this.description;
-        }
-
-        public long getCreate() {
-            return this.create;
+    public long getCreate() {
+        return this.create;
         }
         
-        public String getId() {
-        	return this.id;
+    public String getId() {
+        return this.id;
         }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String addComment(String comment) {
+        if (this.position == this.comment.length)
+        {
+            String[] commentTemp = new String[2 * this.comment.length];
+            System.arraycopy(this.comment, 0, commentTemp, 0, this.comment.length);
+            this.comment = commentTemp;
+        }
+        this.comment[this.position++] = comment;
+        return comment;
+    }
+
+    public String[] getAllComment() {
+        String[] result = new String[this.position];
+        System.arraycopy(this.comment, 0, result, 0, this.position);
+        return result;
+    }
+
 }
