9cc2098a21f6b3c0596818083f0b400fadbc61f5
androsdav <androsdav@gmail.com>
2016-09-21 11:36:22 +0300

    add Item

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/models/Item.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/models/Item.java
@@ -16,40 +16,39 @@ public class Item {
     }
 
     public void setName(String name) {
-            this.name = name;
-        }
+        this.name = name;
+    }
 
     public void setDescription(String description) {
         this.description = description;
-        }
+    }
 
     public void setCreate(long create) {
         this.create = create;
-        }
-        
+    }
+
     public void setId(String id) {
         this.id = id;
-        }
+    }
 
     public String getDescription() {
         return this.description;
-        }
+    }
 
     public long getCreate() {
         return this.create;
-        }
-        
+    }
+
     public String getId() {
         return this.id;
-        }
+    }
 
     public String getName() {
         return this.name;
     }
 
     public String addComment(String comment) {
-        if (this.position == this.comment.length)
-        {
+        if (this.position == this.comment.length) {
             String[] commentTemp = new String[2 * this.comment.length];
             System.arraycopy(this.comment, 0, commentTemp, 0, this.comment.length);
             this.comment = commentTemp;
@@ -57,7 +56,7 @@ public class Item {
         this.comment[this.position++] = comment;
         return comment;
     }
-    
+
     public String[] getAllComment() {
         String[] result = new String[this.position];
         System.arraycopy(this.comment, 0, result, 0, this.position);
