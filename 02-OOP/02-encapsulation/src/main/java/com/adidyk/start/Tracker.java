7ae138cf3ecb64598de45eaf8550c49701d9aebc
androsdav <androsdav@gmail.com>
2016-09-21 11:32:06 +0300

    add Tracker

--- 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
+++ 02-OOP/02-encapsulation/src/main/java/com/adidyk/start/Tracker.java
@@ -4,66 +4,65 @@ import java.util.*;
 
 public class Tracker {
 
-    private Item[] item = new Item[1];
-    private int position = 0;
-    private static final Random RN = new Random();
+	private Item[] item = new Item[1];
+	private int position = 0;
+	private static final Random RN = new Random();
 	private Item[] itemSearch;
 	private Item itemComm;
-    
-    // addItem - add new Item
-    protected Item addItem(Item item) {
-        if (this.position == this.item.length)
-        {
-            Item[] itemTemp = new Item[2 * this.item.length];
-            System.arraycopy(this.item, 0, itemTemp, 0, this.item.length);
-            this.item = itemTemp;
-        }
-        item.setId(this.generateId());
-        this.item[this.position++] = item;
-        return item;
-    }
 
-    // getAllItem - return all Item
-    protected Item[] getAllItem() {
-        Item[] result = new Item[this.position];
-        System.arraycopy(this.item, 0, result, 0, this.position);
-        return result;
-    }
+	// addItem - add new Item
+	protected Item addItem(Item item) {
+		if (this.position == this.item.length) 	{
+			Item[] itemTemp = new Item[2 * this.item.length];
+			System.arraycopy(this.item, 0, itemTemp, 0, this.item.length);
+			this.item = itemTemp;
+		}
+		item.setId(this.generateId());
+		this.item[this.position++] = item;
+		return item;
+	}
 
-    // searchItemById - search Item by id
-    protected Item searchItemById(String id) {
-       	Item result = null;
+	// getAllItem - return all Item
+	protected Item[] getAllItem() {
+		Item[] result = new Item[this.position];
+		System.arraycopy(this.item, 0, result, 0, this.position);
+		return result;
+	}
+
+	// searchItemById - search Item by id
+	protected Item searchItemById(String id) {
+		Item result = null;
 		for (Item item : this.item) {
 			if (item != null && item.getId().equals(id)) {
 				result = item;
-				break; 
+				break;
 			}
 		}
 		this.itemComm = result;
-    	return result;
-    }
+		return result;
+	}
 
-    // searchItemByName - search Item by name
-    protected Item[] searchItemByName(String name) {
+	// searchItemByName - search Item by name
+	protected Item[] searchItemByName(String name) {
 		int length = 0;
 		for (Item item : this.item) {
 			if (item != null && item.getName().equals(name)) {
-				length++;
+			length++;
 			}
 		}
 		Item[] result = new Item[length];
 		int index = 0;
 		for (Item item : this.item) {
 			if (item != null && item.getName().equals(name)) {
-				result[index++] = item;
+			result[index++] = item;
 			}
 		}
 		this.itemSearch = result;
 		return result;
 	}
 
-    // searchItemByDescription - search all Item by description
-    protected Item[] searchItemByDescription(String description) {
+	// searchItemByDescription - search all Item by description
+	protected Item[] searchItemByDescription(String description) {
 		int length = 0;
 		for (Item item : this.item) {
 			if (item != null && item.getDescription().equals(description)) {
@@ -81,31 +80,31 @@ public class Tracker {
 		return result;
 	}
 
-    // searchItemByCreate - search Item by create
-    protected Item searchItemByCreate(long create) {
-    	Item result = null;
-    	for (Item item : this.item) {
-    		if (item != null && item.getCreate() == create) {
-    			result = item;
-    			break;
-    		}
-    	}
-    	return result;
-    }
+	// searchItemByCreate - search Item by create
+	protected Item searchItemByCreate(long create) {
+		Item result = null;
+		for (Item item : this.item) {
+			if (item != null && item.getCreate() == create) {
+				result = item;
+				break;
+			}
+		}
+		return result;
+	}
 
-    // deleteItem - delete Item by id
-    protected void deleteItemById(String id) {
-    	for (int i = 0; i < this.position; i++) {
-    		if (this.item[i].getId().equals(id)) {
-    			for (int j = i; j < this.position - 1; j++) {
-    				this.item[j] = this.item[j + 1];
-    			}
-    			this.item[this.position - 1] = null;
-    			this.position = this.position - 1;
-    			break;
-    		}
-    	}
-    }
+	// deleteItem - delete Item by id
+	protected void deleteItemById(String id) {
+		for (int i = 0; i < this.position; i++) {
+			if (this.item[i].getId().equals(id)) {
+				for (int j = i; j < this.position - 1; j++) {
+					this.item[j] = this.item[j + 1];
+				}
+				this.item[this.position - 1] = null;
+				this.position = this.position - 1;
+				break;
+			}
+		}
+	}
 
 	// renameItemByName - rename all Item by name by this.itemSearch
 	protected void renameItemByName(String name) {
@@ -121,19 +120,19 @@ public class Tracker {
 		}
 	}
 
-	// upadteItemById - update name, description, create by find id
+	// updateItemById - update name, description, create in item by id
 	protected void updateItemById(Item item) {
 		this.itemComm = item;
 	}
 
-    // addCommentById - add comment by id
+	// addCommentById - add comment by id
 	protected void addCommentById(String comment) {
 		this.itemComm.addComment(comment);
 	}
 
 	// generateId - gererate id
-    String generateId() {
-    	return String.valueOf(System.currentTimeMillis() + this.RN.nextInt());
-    }
-    
+	String generateId() {
+		return String.valueOf(System.currentTimeMillis() + this.RN.nextInt());
+	}
+
 }
