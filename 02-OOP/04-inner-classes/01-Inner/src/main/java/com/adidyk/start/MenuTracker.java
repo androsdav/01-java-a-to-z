3b6ff27ef5f20577c36822753e443cd0d31635d5
androsdav <androsdav@gmail.com>
2016-11-02 00:40:38 +0200

    update all

--- 02-OOP/04-inner-classes/01-Inner/src/main/java/com/adidyk/start/MenuTracker.java
+++ 02-OOP/04-inner-classes/01-Inner/src/main/java/com/adidyk/start/MenuTracker.java
@@ -17,8 +17,10 @@ public class MenuTracker {
     }
 
     public void fillAction() {
-                this.actions[0] = new AddItem();
+        this.actions[0] = new AddItem();
         this.actions[1] = new ShowAllItem();
+        this.actions[2] = new SearchItemById();
+        this.actions[3] = new SearchItemByName();
 //        System.out.println(this.actions[0]);
     }
 
@@ -36,30 +38,31 @@ public class MenuTracker {
         System.out.println(" ---------------------------------------------------------------------------");
     }
 
+    // class AddItem,  key = 0
     private class AddItem implements UserAction {
-
+        // key = 0
         public int key() {
             return 0;
         }
-
+        // execute - add new item, key = 0
         public void execute(Input input, Tracker track) {
             String name = input.ask(" Input name item: ");
             String desc = input.ask(" Input desc item: ");
             track.addItem(new Item(name, desc,  new Date().getTime()));
         }
-
+        // info - Add new item
         public String info() {
             return String.format(" %s%s%s", this.key(), ".", " Add new item.");
         }
-
     }
 
+    // class ShowAllItem, key = 1
     private class ShowAllItem implements UserAction {
-
+        // key = 1
         public int key() {
             return 1;
         }
-
+        // execute - show all item, key = 1
         public void execute(Input input, Tracker track) {
             for (Item item : track.getAllItem()) {
                 if (item != null) {
@@ -72,12 +75,63 @@ public class MenuTracker {
                 }
             }
         }
-
+        // info - menu item " Show all item."
         public String info() {
             return String.format(" %s%s%s", this.key(), ".", " Show all item.");
         }
+    }
 
+    // class SearchItemById, key = 2
+    private class SearchItemById implements UserAction {
+        // key = 2
+        public int key() {
+            return 2;
+        }
+        // execute - search item by id, key = 3
+        public void execute(Input input, Tracker track) {
+            String id = input.ask(" Input id: ");
+            Item item = track.searchItemById(id);
+            if (item != null) {
+                System.out.println(item.toString());
+                for (Comment comm : item.getAllComment()) {
+                    if (comm != null) {
+                        System.out.println("   - [comm]: " + comm.getComment());
+                    }
+                }
+            } else {
+                System.out.println(" No result by id");
+            }
+        }
+        // info - menu item " Search item by id."
+        public String info() {
+            return String.format(" %s%s%s", this.key(), ".", " Search item by id.");
+        }
+    }
 
+    // class SearchItemByName, key = 3
+    private class SearchItemByName implements UserAction {
+        // key = 3
+        public int key() {
+            return 3;
+        }
+        // execute - search item by name, key = 3
+        public void execute(Input input, Tracker track) {
+            String name = input.ask(" Input name: ");
+            for (Item item : track.getAllItem()) {
+                if (item != null && item.getName().equals(name)) {
+                    System.out.println(item.toString());
+                    for (Comment comm : item.getAllComment()) {
+                        if (comm != null) {
+                            System.out.println("   - [comm]: " + comm.getComment());
+                        }
+                    }
+                }
+            }
+        }
+        // info - menu item " Search item by name."
+        public String info() {
+            return String.format(" %s%s%s", this.key(), ".", " Search item by name.");
+        }
     }
 
 
