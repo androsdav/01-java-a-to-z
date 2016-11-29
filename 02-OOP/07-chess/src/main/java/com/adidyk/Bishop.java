365620c7a5eebdf413dba5a7590930b7c9d12db7
androsdav <androsdav@gmail.com>
2016-11-30 00:47:34 +0200

    add pom

--- 02-OOP/07-chess/src/main/java/com/adidyk/Bishop.java
+++ 02-OOP/07-chess/src/main/java/com/adidyk/Bishop.java
@@ -2,21 +2,8 @@ package com.adidyk;
 
 public class Bishop extends Figure {
 
-        public Bishop(String color, Cell cell) {
+    public Bishop(String color, Cell cell) {
         super(color, cell);
     }
-
-    //public void setColor(String color) {
-    //    this.color = color;
-    //}
-
-    //public String getColor() {
-    //    return this.color;
-    //}
-
-    //public Cell getCell() {
-    //    return this.cell;
-    //}
-
 
 }
