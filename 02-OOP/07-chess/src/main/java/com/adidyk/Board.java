3d59d06dbc6db7c61a63677332243bc29dfff43b
androsdav <androsdav@gmail.com>
2016-12-07 11:05:25 +0200

    update

--- 02-OOP/07-chess/src/main/java/com/adidyk/Board.java
+++ 02-OOP/07-chess/src/main/java/com/adidyk/Board.java
@@ -2,7 +2,26 @@ package com.adidyk;
 
 public class Board {
 
-    public Figure[] figures;
+    public Figure[] figures = new Figure[32];
+
+    public Figure addFigure(Figure figure) {
+        for(int index = 0; index < this.figures.length; index++) {
+            if(this.figures[index] == null) {
+                this.figures[index] = figure;
+            }
+        }
+        return figure;
+    }
+
+    public void removeFigure(Figure figure) {
+        for(int index = 0; index < this.figures.length; index++) {
+            if(this.figures[index] != null && this.figures[index].getCell().getPositionX() == figure.getCell().getPositionX() ) {
+                this.figures[index] = null;
+                break;
+            }
+        }
+    }
+
 
     public void move(Cell source, Cell dist) {
 
