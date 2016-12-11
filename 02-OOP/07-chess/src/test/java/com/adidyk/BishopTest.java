ebc1b5c6d3d9beef9b3d796eb372f34f3f8330f0
androsdav <androsdav@gmail.com>
2016-12-11 23:35:16 +0200

    update

--- 02-OOP/07-chess/src/test/java/com/adidyk/BishopTest.java
+++ 02-OOP/07-chess/src/test/java/com/adidyk/BishopTest.java
@@ -1,4 +1,17 @@
 package com.adidyk;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class BishopTest {
+
+    @Test
+    public void wayTest() {
+
+        Cell[] highway = {new Cell(6, 5), new Cell(7, 6), new Cell(8, 7)};
+        Bishop bishop = new Bishop("black", new Cell(5, 4));
+        Cell[] result = bishop.way(new Cell(8, 7));
+        assertThat(result, is(highway));
+    }
 }
