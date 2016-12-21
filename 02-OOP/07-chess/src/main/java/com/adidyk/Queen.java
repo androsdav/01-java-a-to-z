0a422d1b3f3e144308994a68a0d7d1fbbd17c128
androsdav <androsdav@gmail.com>
2016-12-21 17:32:13 +0200

    update

---
+++ 02-OOP/07-chess/src/main/java/com/adidyk/Queen.java
@@ -0,0 +1,11 @@
+package com.adidyk;
+
+public class Queen extends Figure{
+
+    // Constructor
+    public Queen(String color, Cell cell) {
+        super(color, cell);
+    }
+
+    // way - will return all way, if way = true
+}
