2e8e120ec98e106506be5162bccf00261192894e
androsdav <androsdav@gmail.com>
2016-12-30 16:46:18 +0200

    add all

--- 03-Input-Output/01-io/01-byte-stream/src/main/java/com/adidyk/StartUi.java
+++ 03-Input-Output/01-io/01-byte-stream/src/main/java/com/adidyk/StartUi.java
@@ -6,47 +6,22 @@ import java.io.*;
 
 public class StartUi {
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) throws IOException {
 
-        try{
+        System.out.println("");
 
-            BufferedReader br =
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "Cp866"));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out, "Cp866"), true);
 
-                    new BufferedReader(new InputStreamReader(System.in, "Cp866"));
+        String s = br.readLine();
+        System.out.println("Output s: " + s);
 
-            PrintWriter pw = new PrintWriter(
 
-                    new OutputStreamWriter(System.out, "Cp866"), true);
 
-            String s = "Это строка с русским текстом";
 
-            System.out.println("System.out puts: " + s);
 
-            pw.println("PrintWriter puts: " + s) ;
 
-            int с = 0;
-
-            pw.println("Посимвольный ввод:");
-
-            while((с = br.read()) != -1)
-
-                pw.println((char)c);
-
-            pw.println("Построчный ввод:");
-
-            do{
-
-                s = br.readLine();
-
-                pw.println(s);
-
-            }while(!s.equals("q"));
-
-        }catch(Exception e){
-
-            System.out.println(e);
-
-        }
+    }
 //        int c = 0;
  //       pw.println((char)c);
         //String symbol = br.read((char)c);
@@ -143,6 +118,6 @@ public class StartUi {
         System.out.println(c==d);
 
         Bishop bishop = new Bishop("black", new Cell(1, 1));*/
-    }
+
 
 }
