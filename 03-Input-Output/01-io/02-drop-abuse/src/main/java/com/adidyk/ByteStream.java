a335ac615c2e06f90a3d418bd4ff3e3013e4e7e6
androsdav <androsdav@gmail.com>
2017-01-04 01:19:47 +0200

    add

---
+++ 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/ByteStream.java
@@ -0,0 +1,27 @@
+package com.adidyk;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import static java.lang.Math.IEEEremainder;
+
+public class ByteStream {
+
+    // isNumber - checks that byte stream have only an even number
+    public boolean isNumber(InputStream in) {
+        boolean numberFlag = false;
+        try (BufferedInputStream bis = new BufferedInputStream(in)) {
+            byte[] buffer = new byte[bis.available()];
+            bis.read(buffer, 0, buffer.length);
+            Float number = Float.valueOf(new String(buffer, "cp1251"));
+            double remainder = IEEEremainder(number, 2.0);
+            if (remainder == 0) {
+                numberFlag = true;
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return numberFlag;
+    }
+
+}
---
+++ 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/StartUi.java
@@ -0,0 +1,18 @@
+package com.adidyk;
+
+import java.io.*;
+
+public class StartUi {
+
+    // main - input number from console
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "Cp866"));
+        System.out.print("Input even or odd number: ");
+        String text = br.readLine();
+        byte[] buffer = text.getBytes();
+        ByteStream bs = new ByteStream();
+        boolean number = bs.isNumber(new ByteArrayInputStream(buffer));
+        System.out.println("Even number: " + number);
+    }
+
+}
