c06cb11d6dffb5ccc673914a18b9152d14255b91
androsdav <androsdav@gmail.com>
2017-01-05 21:40:13 +0200

    add

--- 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/RemoveAbuse.java
+++ 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/RemoveAbuse.java
@@ -7,23 +7,15 @@ public class RemoveAbuse {
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in)); BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out))) {
             String text;
-            do {
-//            while (!(text = br.readLine()).equals("q")) {
-                text = br.readLine();
-                String[] args = text.split(" ");
-                for (int arg = 0; arg < args.length; arg++) {
-                    for (String abu : abuse) {
-                        if (args[arg].equals(abu)) {
-                            args[arg] = " ";
-                        }
+            text = br.readLine();
+            String[] args = text.split(" ");
+            for (String arg : args) {
+                for (String abu : abuse) {
+                    if (!arg.equals(abu)) {
+                        bw.write(arg +" ");
                     }
                 }
-                for (String arg : args) {
-                    bw.write(arg + " ");
-                }
-
             }
-            while (!text.equals("q"));
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
--- 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/StartUi.java
+++ 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/StartUi.java
@@ -7,9 +7,34 @@ public class StartUi {
     // main - input row from console
     public static void main(String[] args) throws IOException {
         RemoveAbuse ra = new RemoveAbuse();
-        String[] abuse = {"abuse"};
+        String[] abuse = {"abuse1", "abuse2"};
         System.out.println("Input row: ");
         ra.dropAbuses(System.in, System.out, abuse);
     }
+
+
+/*    do {
+//            while (!(text = br.readLine()).equals("q")) {
+        text = br.readLine();
+        String[] args = text.split(" ");
+        for (int arg = 0; arg < args.length; arg++) {
+            for (String abu : abuse) {
+                if (args[arg].equals(abu)) {
+                    args[arg] = " ";
+                }
+            }
+        }
+        for (String arg : args) {
+            bw.write(arg + " ");
+        }
+
+    }
+    while (!text.equals("q"));
+}
+catch (Exception ex) {
+        System.out.println(ex.getMessage());
+        }
+
+    */
 
 }
