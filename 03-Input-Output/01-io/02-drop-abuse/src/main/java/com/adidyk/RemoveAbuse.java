aa937f024925159525df85ae6331e5ad399dbdae
androsdav <androsdav@gmail.com>
2017-01-05 17:37:31 +0200

    update

--- 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/RemoveAbuse.java
+++ 03-Input-Output/01-io/02-drop-abuse/src/main/java/com/adidyk/RemoveAbuse.java
@@ -7,7 +7,9 @@ public class RemoveAbuse {
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in)); BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out))) {
             String text;
-            while (!(text = br.readLine()).equals("q")) {
+            do {
+//            while (!(text = br.readLine()).equals("q")) {
+                text = br.readLine();
                 String[] args = text.split(" ");
                 for (int arg = 0; arg < args.length; arg++) {
                     for (String abu : abuse) {
@@ -17,9 +19,11 @@ public class RemoveAbuse {
                     }
                 }
                 for (String arg : args) {
-                    System.out.print(arg);
+                    bw.write(arg);
                 }
+
             }
+            while (!text.equals("q"));
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
