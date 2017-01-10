9b79fa1127bdba7c91185cf3bab328cd589955ab
androsdav <androsdav@gmail.com>
2017-01-10 17:25:23 +0200

    add new

--- 03-Input-Output/01-io/03-sort-3G/src/main/java/com/adidyk/StartUi.java
+++ 03-Input-Output/01-io/03-sort-3G/src/main/java/com/adidyk/StartUi.java
@@ -1,16 +1,46 @@
 package com.adidyk;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class StartUi {
 
     // main - input several row from console, for finished input <q>
     public static void main(String[] args) throws IOException {
-
         SortFile sf = new SortFile();
         File source = new File("source.txt");
+        File dist = new File("dist.txt");
+
+
         sf.sort(source);
+        System.out.println(source.length());
+        System.out.println(source.getAbsoluteFile());
+        System.out.println(source.getAbsolutePath());
+        System.out.println(source.getParent());
+        System.out.println(source.getParentFile());
+    }
+}
+
+         /*   try(BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt")))
+            {
+                // чтение построчно
+                String text;
+                while(!(text = br.readLine()).equals("ESC")){
+
+                    bw.write(text);
+                    bw.flush();
+                }
+            }
+            catch(IOException ex){
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    } */
+        //SortFile sf = new SortFile();
+        //File source = new File("source.txt");
+        //File dist = new File("dist");
+        //sf.sort(source);
         //System.getProperty("user.dir");
        /* RemoveAbuse ra = new RemoveAbuse();
         String[] abuse = {"abuse1", "abuse2", "abuse3"};
@@ -27,6 +57,3 @@ public class StartUi {
             row = row.concat(arg).concat(" ");
         }
         ra.dropAbuses(new ByteArrayInputStream(row.getBytes()), System.out, abuse);*/
-    }
-
-}
