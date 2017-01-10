a9839dba8762e8bd0f8f44e61362873db2c25992
androsdav <androsdav@gmail.com>
2017-01-10 10:56:49 +0200

    add new

---
+++ 03-Input-Output/01-io/03-sort-3G/src/main/java/com/adidyk/StartUi.java
@@ -0,0 +1,26 @@
+package com.adidyk;
+
+import java.io.IOException;
+
+public class StartUi {
+
+    // main - input several row from console, for finished input <q>
+    public static void main(String[] args) throws IOException {
+       /* RemoveAbuse ra = new RemoveAbuse();
+        String[] abuse = {"abuse1", "abuse2", "abuse3"};
+        String text;
+        String row = "";
+        ArrayList<String> list = new ArrayList<>();
+        System.out.println("Input several row, for finished input <q>: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (!(text = br.readLine()).equals("q")) {
+            list.add(text);
+        }
+        br.close();
+        for (String arg : list) {
+            row = row.concat(arg).concat(" ");
+        }
+        ra.dropAbuses(new ByteArrayInputStream(row.getBytes()), System.out, abuse);*/
+    }
+
+}
---
+++ 03-Input-Output/01-io/03-sort-3G/src/test/java/com/adidyk/SortFileTest.java
@@ -0,0 +1,4 @@
+package com.adidyk;
+
+public class SortFileTest {
+}
