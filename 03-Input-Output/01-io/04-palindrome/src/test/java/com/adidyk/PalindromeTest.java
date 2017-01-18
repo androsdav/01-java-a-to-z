4dfcdaa8099ca579a61e7ee8e6e01232f2b6cc8b
androsdav <androsdav@gmail.com>
2017-01-18 17:26:58 +0200

    update

--- 03-Input-Output/01-io/04-palindrome/src/test/java/com/adidyk/PalindromeTest.java
+++ 03-Input-Output/01-io/04-palindrome/src/test/java/com/adidyk/PalindromeTest.java
@@ -2,12 +2,20 @@ package com.adidyk;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 public class PalindromeTest {
 
     @Test
-    public void palindromeCheckTest() {
+    public void palindromeCheckTest() throws IOException {
 
         String word = "komok";
+        Palindrome palindrome = new Palindrome();
+        boolean valid = palindrome.palindromeCheck(new ByteArrayInputStream(word.getBytes()));
+        assertTrue(valid);
 
     }
 
