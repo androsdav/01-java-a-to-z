02c237fcbcd7618f5c269c4a1dfc7c2afce301b5
androsdav <androsdav@gmail.com>
2017-01-23 01:04:50 +0200

    update

--- 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/ChatConsole.java
+++ 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/ChatConsole.java
@@ -1,42 +1,40 @@
 package com.adidyk;
 
 import java.io.*;
-import java.util.Map;
-
 import static java.lang.Math.random;
 
 public class ChatConsole {
 
-    public void chat(InputStream in, File ans, File log) throws IOException {
+    private final String FINISH = "finish", STOP = "stop", PLAY = "play";
 
+    // chat - just do all for console chat
+    public void chat(InputStream in, File ans, File log) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in));
         RandomAccessFile rafAnswer = new RandomAccessFile(ans, "rw");
         RandomAccessFile rafLog = new RandomAccessFile(log, "rw")) {
             String question;
             String answer;
-            boolean flag = false;
-            String[] rows = this.answerRandom(rafAnswer);
-            for (String row : rows) {
-                System.out.print(row + " ");
-            }
-            while (!(question = br.readLine()).equals("finish")) {
-                if (question.equals("stop")) {
-                    flag = true;
+            boolean flagStop = false;
+            String[] rows = this.getPositionAllRow(rafAnswer);
+            System.out.print("[question]: ");
+            while (!(question = br.readLine()).equals(this.FINISH)) {
+                if (question.equals(this.STOP)) {
+                    flagStop = true;
                 }
-                if (question.equals("play")) {
-                    flag = false;
+                if (question.equals(this.PLAY)) {
+                    flagStop = false;
                 }
-                if (!flag) {
+                if (!flagStop) {
                     long pos = (long)(random() * rows.length - 1);
                     rafAnswer.seek(Long.valueOf(rows[(int)(pos)]));
-                    System.out.println(pos);
                     answer = rafAnswer.readLine();
-                    System.out.println(answer);
+                    System.out.println("[answer]:   " + answer);
                     rafLog.writeBytes("[question]: " + question.concat(System.lineSeparator()));
                     rafLog.writeBytes("[answer]:   " + answer.concat(System.lineSeparator()));
                 } else{
                     rafLog.writeBytes("[question]: " + question.concat(System.lineSeparator()));
                 }
+                System.out.print("[question]: ");
             }
             rafLog.writeBytes("[question]: " + question.concat(System.lineSeparator()));
         }
@@ -45,14 +43,13 @@ public class ChatConsole {
         }
     }
 
-    //
-    private String[] answerRandom(RandomAccessFile raf) throws IOException {
-        String line = "0 ";
+    // getPositionAllRow - return positions for all rows from file = "answer.txt"
+    private String[] getPositionAllRow(RandomAccessFile raf) throws IOException {
+        String line = "0".concat(System.lineSeparator());
         while ((raf.readLine()) != null) {
-            line = line.concat(Long.toString(raf.getFilePointer())).concat(" ");
+            line = line.concat(Long.toString(raf.getFilePointer())).concat(System.lineSeparator());
         }
-        String[] rows = line.split(" ");
-        return rows;
+        return line.split(System.lineSeparator());
     }
 
 }
--- 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/StartUi.java
+++ 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/StartUi.java
@@ -10,23 +10,6 @@ public class StartUi {
         File log = new File("log.txt");
         ChatConsole chatConsole = new ChatConsole();
         chatConsole.chat(System.in,answer, log);
-       // try (RandomAccessFile raf = new RandomAccessFile(answer, "rw")) {
-       //     chatConsole.answerRandom(raf);
-       // }
-       // catch (Exception ex) {
-       //     ex.getMessage();
-       // }
-
-
-/*        System.out.println("Input word - palindrome: ");
-        try {
-            Palindrome palindrome = new Palindrome();
-            System.out.println(palindrome.palindromeCheck(System.in));
-        }
-        catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }*/
-        //System.out.println("Hello Word !!!");
     }
 
 }
