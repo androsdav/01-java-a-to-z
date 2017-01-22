5ef4aa83c12a90122f7a1f6287b4b2410acd83c7
androsdav <androsdav@gmail.com>
2017-01-22 14:59:44 +0200

    update

--- 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/ChatConsole.java
+++ 03-Input-Output/01-io/05-chat-console/src/main/java/com/adidyk/ChatConsole.java
@@ -6,14 +6,12 @@ public class ChatConsole {
 
     public void chat(InputStream in, OutputStream out, File answer, File log) {
 
-//        File fileLog = new File("log.txt");
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(in)); RandomAccessFile raf = new RandomAccessFile(answer, "rw")) {
 
-            System.out.println("Input question: ");
-            br.readLine();
-            String row = raf.readLine();
-            System.out.println(row);
+            //String row;
+            while (br.readLine() != "finish") {
+                System.out.println(raf.readLine());
+            }
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
