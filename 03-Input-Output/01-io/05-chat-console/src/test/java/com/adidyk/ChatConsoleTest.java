37aef2cc257948b7cf57796e23ba4518500ab3e0
androsdav <androsdav@gmail.com>
2017-01-23 21:57:50 +0200

    update

--- 03-Input-Output/01-io/05-chat-console/src/test/java/com/adidyk/ChatConsoleTest.java
+++ 03-Input-Output/01-io/05-chat-console/src/test/java/com/adidyk/ChatConsoleTest.java
@@ -9,8 +9,8 @@ public class ChatConsoleTest {
     @Test
     public void chatTest() throws IOException {
 
-        String[] answerSource = {"yes", "yes", "yes", "yes"};
-        String[] questionSource = {"Do you work ?", "Does she do exercise ?", "Did you see her ?"};
+        String[] answerSource = {"I do not know", "no", "maybe", "nothing"};
+        String[] questionSource = {"Do you work ?", "Does she do exercise ?", "Did you see her ?", "Do you like it ?"};
 
         File answer = new File("answerTest.txt");
         File quest = new File("questionTest.txt");
@@ -26,16 +26,38 @@ public class ChatConsoleTest {
             System.out.println(ex.getMessage());
         }
 
-        try (RandomAccessFile rafQuest = new RandomAccessFile(quest, "rw")) {
-            for (String question : questionSource) {
-                rafQuest.writeBytes(question.concat(System.lineSeparator()));
-            }
+        String line = "";
+        for (String question : questionSource) {
+            line = line.concat(question).concat(System.lineSeparator());
         }
-        catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        ChatConsole chatCons = new ChatConsole();
+        chatCons.chat(new ByteArrayInputStream(line.getBytes()), answer, log);
 
+        //
+       // try (RandomAccessFile rafQuest = new RandomAccessFile(quest, "rw")) {
+       //     for (String question : questionSource) {
+       //         rafQuest.writeBytes(question.concat(System.lineSeparator()));
+       //     }
+       // }
+       // catch (Exception ex) {
+       //     System.out.println(ex.getMessage());
+       // }
+
+
+
+        //
         //ChatConsole chatConsole = new ChatConsole();
+    //    try (RandomAccessFile raf = new RandomAccessFile(quest, "r")) {
+    //        String row;
+    //        ChatConsole chatConsole = new ChatConsole();
+    //        while ((row = raf.readLine()) != null)  {
+    //            chatConsole.chat(new ByteArrayInputStream(row.getBytes()), answer, log);
+    //        }
+
+    //    }
+    //    catch (Exception ex) {
+    //        System.out.println(ex.getMessage());
+    //    }
         //for (String question : questionSource) {
         //    System.out.println(question + "test");
         //    chatConsole.chat(new BufferedReader(new InputStreamReader(question)), answer, log);
