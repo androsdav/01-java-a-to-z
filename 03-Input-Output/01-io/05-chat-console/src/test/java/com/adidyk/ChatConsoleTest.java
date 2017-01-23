010a7018075e7279348e23f25bc5a0d1ce820566
androsdav <androsdav@gmail.com>
2017-01-23 17:48:57 +0200

    new project

--- 03-Input-Output/01-io/05-chat-console/src/test/java/com/adidyk/ChatConsoleTest.java
+++ 03-Input-Output/01-io/05-chat-console/src/test/java/com/adidyk/ChatConsoleTest.java
@@ -1,5 +1,36 @@
 package com.adidyk;
 
+import org.junit.Test;
+
+import java.io.*;
+
 public class ChatConsoleTest {
+
+    @Test
+    public void chatTest() throws IOException {
+
+        String[] answerSource = {"yes", "yes", "yes", "yes"};
+        String[] questionSource = {"Do you work ?", "Does she do exercise ?", "Did you see her ?"};
+        File answer = new File("answerTest.txt");
+        File log = new File("logTest.txt");
+        // create file answerTest.txt
+        try (RandomAccessFile raf = new RandomAccessFile(answer, "rw")) {
+            for (String ans : answerSource) {
+                raf.writeBytes(ans.concat(System.lineSeparator()));
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        ChatConsole chatConsole = new ChatConsole();
+        for (String question : questionSource) {
+            System.out.println(question + "test");
+            chatConsole.chat(new ByteArrayInputStream(question.getBytes()), answer, log);
+        }
+
+
+
+
+    }
 
 }
