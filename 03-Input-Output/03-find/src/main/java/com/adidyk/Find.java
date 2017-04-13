d18be3a56dd36ebdc78b005d1f7ab7da68595c2c
androsdav <androsdav@gmail.com>
2017-04-14 01:44:15 +0300

    update

--- 03-Input-Output/03-find/src/main/java/com/adidyk/Find.java
+++ 03-Input-Output/03-find/src/main/java/com/adidyk/Find.java
@@ -1,7 +1,28 @@
 package com.adidyk;
 
-/**
- * Created by Nastya on 13.04.17.
- */
+import java.io.File;
+
 public class Find {
+
+    public void find(String root) {
+        String name = "pom.txt";
+        File file = new File(root);
+        boolean findTrue = false;
+        File[] items = file.listFiles();
+        if (items != null) {
+            for (File item : items) {
+                if (item.isDirectory()) {
+                    find(item.getPath());
+                } else if (item.isFile()) {
+                    if (name.equals(item.getName())) {
+                        findTrue = true;
+                        System.out.println(item.getPath());
+                    }
+                }
+            }
+        }
+        //if (!findTrue) {
+        //    System.out.println("Find result: file not found");
+        //}
+    }
 }
--- 03-Input-Output/03-find/src/main/java/com/adidyk/StartUi.java
+++ 03-Input-Output/03-find/src/main/java/com/adidyk/StartUi.java
@@ -3,7 +3,10 @@ package com.adidyk;
 public class StartUi {
 
     public static void main(String[] arg) {
-        System.out.println("Test, Hello Word");
+        //System.out.println("Test, Hello Word");
+
+        Find find = new Find();
+        find.find("root");
     }
 
 }
