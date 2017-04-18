dad86cf1c0e2c5774db3921ef09be9855994c0f1
androsdav <androsdav@gmail.com>
2017-04-18 17:42:30 +0300

    update

--- 03-Input-Output/03-find/src/main/java/com/adidyk/Filter.java
+++ 03-Input-Output/03-find/src/main/java/com/adidyk/Filter.java
@@ -5,32 +5,13 @@ import java.io.FilenameFilter;
 
 public class Filter implements FilenameFilter {
 
-   // public static FilenameFilter filter;
 
-   // public static void init() {
-   //     filter = new FilenameFilter() {
-   //         @Override
-   //         public boolean accept(File dir, String name) {
-
-   //             if (name.equals("*dir")) {
-   //                 return (new File(dir, name).isDirectory());
-   //             } else {
-   //                 return false;
-   //             }
-   //          //   if ((dir.isFile() && name.equals("pom.txt")) || dir.isDirectory()) {
-             //       return true;
-             //   } else {
-             //       return false;
-             //   }
-    //        }
-      //  };
-    //}
-
-    String mask;
+    private String mask;
 
     Filter(String mask) {
         this.mask = mask;
     }
+
 
     @Override
     public boolean accept(File dir, String name) {
@@ -46,11 +27,12 @@ public class Filter implements FilenameFilter {
         }
 
         else if (mask.equals("2")) {
-            if (name.equals("pom.txt")) {
+            if (name.equals("123.txt")) {
                 return true;
             }
         }
 
        return new File(dir, name).isDirectory();
     }
+
 }
