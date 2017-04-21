96d344ddd4fe5dd28588164bd8e7ed42ea9c4496
androsdav <androsdav@gmail.com>
2017-04-22 00:16:50 +0300

    update

--- 03-Input-Output/03-find/src/main/java/com/adidyk/exceptions/IncorrectInputPath.java
+++ 03-Input-Output/03-find/src/main/java/com/adidyk/exceptions/IncorrectInputPath.java
@@ -1,9 +1,9 @@
 package com.adidyk.exceptions;
 
-/**
- * Created by Nastya on 21.04.17.
- */
-public class IncorrectInputPath extends Throwable {
-    public IncorrectInputPath(String s) {
+public class IncorrectInputPath extends RuntimeException {
+
+    public IncorrectInputPath(String msg) {
+        super(msg);
     }
+
 }
--- 03-Input-Output/03-find/src/main/java/com/adidyk/start/Tracker.java
+++ 03-Input-Output/03-find/src/main/java/com/adidyk/start/Tracker.java
@@ -3,11 +3,14 @@ package com.adidyk.start;
 import com.adidyk.exceptions.IncorrectInputPath;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class Tracker {
 
-    private String path;
+    private String path = null;
+    private ArrayList<String> result = new ArrayList<>();
 
+    // setPath -
     public String setPath(String path) throws IncorrectInputPath {
         File file = new File(path);
         if (file.exists() && file.isDirectory()) {
@@ -18,7 +21,20 @@ public class Tracker {
         return path;
     }
 
-    public void
+    // find -
+    public void find(String nameFile, String keyFind) {
+        File file = new File(this.path);
+        File[] items = file.listFiles();
+        if (items != null) {
+            for (File item : items) {
+                if (item.isDirectory()) {
+                    find(item.getParent(), keyFind);
+                } else if (item.isFile()) {
+                    this.result.add(item.getPath());
+                }
+            }
+        }
+    }
 
     public String getPath() {
         return this.path;
