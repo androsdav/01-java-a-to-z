3fae94aa382fbdc1e3984e84bd64a4a527bfd96c
androsdav <androsdav@gmail.com>
2017-05-09 19:12:55 +0300

    update

--- 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
@@ -4,6 +4,7 @@ public class StartUi {
 
     public static void main(String[] arg) {
         System.out.println("Test begin to start new project now !!!");
+        System.out.println("Add new ");
     }
 
 }
