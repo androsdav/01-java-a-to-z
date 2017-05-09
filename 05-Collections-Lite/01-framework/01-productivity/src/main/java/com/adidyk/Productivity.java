f0672c4850ca0148efbbee631fbc7c7603a083cc
androsdav <androsdav@gmail.com>
2017-05-09 21:23:08 +0300

    update

---
+++ 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/Productivity.java
@@ -0,0 +1,26 @@
+package com.adidyk;
+
+import java.util.Collection;
+
+public class Productivity {
+
+    public long add(Collection<String> collection, String line, int amount) {
+        long start = System.nanoTime();
+        for (int index = 0; index < amount; index++) {
+            collection.add(line);
+        }
+        long end = System.nanoTime();
+        return end - start;
+    }
+
+    public long delete(Collection<String> collection, int amount) {
+        long start = System.nanoTime();
+        for(int index = 0; index < amount; index ++) {
+            collection.remove(index);
+        }
+        long ned = System.nanoTime();
+        return ned - start;
+    }
+
+
+}
--- 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
@@ -1,10 +1,38 @@
 package com.adidyk;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
 public class StartUi {
 
     public static void main(String[] arg) {
         System.out.println("Test begin to start new project now !!!");
         System.out.println("Add new ");
+        String line = "test line";
+        int amount = 1000000;
+        LinkedList<String> linkedList = new LinkedList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        ArrayList<String> arrayList3 = new ArrayList<>();
+
+        TreeSet<String> treeSet = new TreeSet<>();
+        TreeSet<String> treeSet1 = new TreeSet<>();
+
+        Productivity product = new Productivity();
+        System.out.println("ArrayList  :  " + product.add(arrayList, line, amount) + " [ns]");
+        System.out.println("TreeSet    :  " + product.add(treeSet, line, amount) + " [ns]");
+        System.out.println("TreeSet1:  :  " + product.add(treeSet1, line, amount) + " [ns]");
+        System.out.println("ArrayList1 :  " + product.add(arrayList1, line, amount) + " [ns]");
+        System.out.println("ArrayList2 :  " + product.add(arrayList2, line, amount) + " [ns]");
+        System.out.println("ArrayList3 :  " + product.add(arrayList3, line, amount) + " [ns]");
+
+        System.out.println("LinkedList :  " + product.add(linkedList, line, amount) + " [ns]");
+
+        //System.out.println("TreeSet:  " + product.add(arrayList2, line, amount) + " [ns]");
+        //System.out.println("TreeSet:  " + product.add(arrayList3, line, amount) + " [ns]");
+        //System.out.println("LinkedList:  " + product.add(arrayList, line, amount) + " [ns]");
     }
 
 }
