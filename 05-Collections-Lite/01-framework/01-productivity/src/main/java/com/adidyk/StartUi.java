be89db963388efd73232a9a4d7025a74023da7ed
androsdav <androsdav@gmail.com>
2017-05-12 16:07:47 +0300

    update

--- 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
@@ -49,25 +49,26 @@ public class StartUi {
         //         System.out.println(item);
         //    }
 
-        System.out.println("LinkedList: " + (float) this.product.delete(this.linkedList, amount / 2) + " [ns]");
-        System.out.println("ArrayList:  " + (float) this.product.delete(this.arrayList, amount / 2) + " [ns]");
-        System.out.println("TreeSet:    " + (float) this.product.delete(this.treeSet, amount / 2) + " [ns]");
+        int amount1 = 50;
+        System.out.println("LinkedList: " + (float) this.product.delete(this.linkedList, amount1) + " [ns]");
+        System.out.println("ArrayList:  " + (float) this.product.delete(this.arrayList, amount1) + " [ns]");
+        System.out.println("TreeSet:    " + (float) this.product.delete(this.treeSet, amount1) + " [ns]");
 
 
-        System.out.println("---------------------ArrayList");
-        for (String item : this.arrayList) {
-            System.out.println(item);
-        }
+       // System.out.println("---------------------ArrayList");
+       // for (String item : this.arrayList) {
+        //    System.out.println(item);
+       // }
 
-        System.out.println("---------------------LinkedId");
-        for (String item : this.linkedList) {
-            System.out.println(item);
-        }
+//        System.out.println("---------------------LinkedId");
+  //      for (String item : this.linkedList) {
+    //        System.out.println(item);
+      //  }
 
-        System.out.println("---------------------TreeSet");
-        for (String item : this.treeSet) {
-            System.out.println(item);
-        }
+        //System.out.println("---------------------TreeSet");
+        //for (String item : this.treeSet) {
+        //    System.out.println(item);
+        //}
     }
     public static void main(String[] arg) {
         Input input = new ConsoleInput();
