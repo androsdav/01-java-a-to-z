e04fc950b1438654cdf4beafab358b32bcff0fb5
androsdav <androsdav@gmail.com>
2017-05-16 01:18:37 +0300

    update

--- 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Lite/01-framework/01-productivity/src/main/java/com/adidyk/StartUi.java
@@ -1,31 +1,17 @@
 package com.adidyk;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.TreeSet;
-
-import static java.lang.Math.pow;
+import java.util.*;
+import static java.lang.Math.*;
 
 public class StartUi {
-
-    private ArrayList<String> warmUpArrayList;
-    private LinkedList<String> warmUpLinkedList;
-    private TreeSet<String> warmUpTreeSet;
 
     private LinkedList<String> linkedList;
     private ArrayList<String> arrayList;
     private Set<String> treeSet;
     private Productivity product;
-    private Input input;
+    private static final double TRANS = pow(10, 9);
 
-    private StartUi(Input input) {
-        //this.input = input;
-
-        this.warmUpArrayList = new ArrayList<>();
-        this.warmUpLinkedList = new LinkedList<>();
-        this.warmUpTreeSet = new TreeSet<>();
-
+    private StartUi() {
         this.linkedList = new LinkedList<>();
         this.arrayList = new ArrayList<>();
         this.treeSet = new TreeSet<>();
@@ -37,85 +23,29 @@ public class StartUi {
     }
 
     private void work() {
-
         int amountAdd = 500000;
         int amountInsert = 5000;
         int amountDelete = 5000;
-
         String stringAdd = "stringAdd";
         String stringInsert = "stringInsert";
 
-        System.out.println("---------------------ArrayList add");
-        System.out.println("ArrayList add:  " + (double)this.product.add(this.arrayList, stringAdd, amountAdd) / pow(10, 9) + " [s]");
-        System.out.println("---------------------ArrayList insert");
-        System.out.println("ArrayList insert: " + (double)this.product.insert(this.arrayList, stringInsert, amountInsert) / pow(10, 9) + " [s]");
-        System.out.println("---------------------ArrayList delete");
-        System.out.println("ArrayList delete: " + (float)this.product.delete(this.arrayList, amountDelete) / pow(10, 9) + " [s]");
+        System.out.println("\n--------------------- LinkedList ---------------------\n");
+        System.out.println("add:    " + this.product.add(this.linkedList, stringAdd, amountAdd) / TRANS + " [s]");
+        System.out.println("insert: " + this.product.insert(this.linkedList, stringInsert, amountInsert) / TRANS + " [s]");
+        System.out.println("delete: " + this.product.delete(this.linkedList, amountDelete) / TRANS + " [s]");
 
-        System.out.println("---------------------LinkedList add");
-        System.out.println("ArrayList add:  " + (double)this.product.add(this.linkedList, stringAdd, amountAdd) / pow(10, 9) + " [s]");
-        System.out.println("---------------------LinkedList insert");
-        System.out.println("ArrayList insert: " + (double)this.product.insert(this.linkedList, stringInsert, amountInsert) / pow(10, 9) + " [s]");
-        System.out.println("---------------------LinkedList delete");
-        System.out.println("ArrayList delete: " + (float)this.product.delete(this.linkedList, amountDelete) / pow(10, 9) + " [s]");
+        System.out.println("\n--------------------- ArrayList ---------------------\n");
+        System.out.println("add:    " + this.product.add(this.arrayList, stringAdd, amountAdd) / TRANS + " [s]");
+        System.out.println("insert: " + this.product.insert(this.arrayList, stringInsert, amountInsert) / TRANS + " [s]");
+        System.out.println("delete: " + this.product.delete(this.arrayList, amountDelete) / TRANS + " [s]");
 
-
-
-        //System.out.println(product.randomString());
-
-//        int amount = Integer.parseInt(this.input.ask("Input amount: "));
-
-//        System.out.println("warm - Up LinkedList:  " + (float) this.product.add(this.warmUpLinkedList, string, amount) + " [ns]");
-   //     System.out.println("        TreeSet:    " + (float) this.product.add(this.treeSet, string, amount) + " [ns]");
-   //     System.out.println("        ArrayList:  " + (float) this.product.add(this.arrayList, string, amount) + " [ns]");
-   //     System.out.println("        LinkedList: " + (float) this.product.add(this.linkedList, string, amount) + " [ns]");
-
-//        System.out.println("warm - Up ArrayList:  " + (float) this.product.add(this.warmUpArrayList, string, amount) + " [ns]");
-//        System.out.println("        ArrayList:  " + (float) this.product.add(this.arrayList, string, amount) + " [ns]");
-
-//        System.out.println("warm - Up TreeSet: " + (float) this.product.add(this.warmUpTreeSet, string, amount) + " [ns]");
-//        System.out.println("        TreeSet:    " + (float) this.product.add(this.treeSet, string, amount) + " [ns]");
-
-   //     System.out.println();
-        //      for (String item : this.arrayList) {
-        //          System.out.println(item);
-        //      }
-
-        //     System.out.println();
-        //      for (String item : this.linkedList) {
-        //         System.out.println(item);
-        //      }
-
-        //     System.out.println();
-        //     for (String item : this.treeSet) {
-        //         System.out.println(item);
-        //    }
-
-   //     int amount1 = 50;
-//        System.out.println("ArrayList:  " + (float) this.product.delete(this.arrayList, amount1) + " [ns]");
-//        System.out.println("LinkedList: " + (float) this.product.delete(this.linkedList, amount1) + " [ns]");
-//        System.out.println("ArrayList:  " + (float) this.product.delete(this.arrayList, amount1) + " [ns]");
-//        System.out.println("TreeSet:    " + (float) this.product.delete(this.treeSet, amount1) + " [ns]");
-
-
-       // System.out.println("---------------------ArrayList");
-       // for (String item : this.arrayList) {
-        //    System.out.println(item);
-       // }
-
-//        System.out.println("---------------------LinkedId");
-  //      for (String item : this.linkedList) {
-    //        System.out.println(item);
-      //  }
-
-        //System.out.println("---------------------TreeSet");
-        //for (String item : this.treeSet) {
-        //    System.out.println(item);
-        //}
+        System.out.println("\n--------------------- TreeSet ---------------------\n");
+        System.out.println("add:    " + this.product.add(this.treeSet, stringAdd, amountAdd) / TRANS + " [s]");
+        System.out.println("delete: " + this.product.delete(this.treeSet, amountDelete) / TRANS + " [s]");
     }
+
     public static void main(String[] arg) {
-        Input input = new ConsoleInput();
-        new StartUi(input).start();
+        new StartUi().start();
     }
 
 }
