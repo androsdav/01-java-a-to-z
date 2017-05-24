15a6c2d159b69d6c84585f32b9d249f571764a0c
androsdav <androsdav@gmail.com>
2017-05-25 00:12:01 +0300

    update

--- 05-Collections-Lite/03-sorting/01-comparable/src/main/java/com/adidyk/SortUser.java
+++ 05-Collections-Lite/03-sorting/01-comparable/src/main/java/com/adidyk/SortUser.java
@@ -1,12 +1,29 @@
 package com.adidyk;
 
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class SortUser {
 
-    public Set<User> sort(List<User> list) {
+    private Set<User> result = new TreeSet<>();
 
-        Set<User> tree = new TreeSet<User>(Arrays.asList(list));
-
+    public Set<User> sortAddUserByFor(List<User> users) {
+        for (User user : users) {
+            this.result.add(user);
+        }
+        return this.result;
     }
+
+    public Set<User> sortAddUserByAddAll(List<User> users) {
+        this.result.addAll(users);
+        return this.result;
+    }
+
+    public Set<User> sortAddUserByConstructor(List<User> users) {
+        return new TreeSet<>(users);
+    }
+
+
+
 }
--- 05-Collections-Lite/03-sorting/01-comparable/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Lite/03-sorting/01-comparable/src/main/java/com/adidyk/StartUi.java
@@ -1,13 +1,16 @@
 package com.adidyk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class StartUi {
 
     public static void main(String[] arg) {
 
-        Set<User> users = new TreeSet<>();
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+
         users.add(new User("antony", 32));
         users.add(new User("bill", 45));
         users.add(new User("bob", 15));
@@ -15,24 +18,30 @@ public class StartUi {
         users.add(new User("will", 10));
         users.add(new User("andrey", 35));
 
+        Set<User> resultFirst = sortUser.sortAddUserByFor(users);
 
         System.out.println();
-
-        System.out.println("Classic loop for-ich");
-        for (User user : users) {
+        System.out.println(" Result treeSet -> (used add user in for):");
+        System.out.println();
+        for (User user : resultFirst) {
             System.out.println(user);
         }
 
         System.out.println();
-
-        System.out.println("Classic loop for-ich with toString");
-        for (User user : users) {
+        System.out.println(" Result treeSet -> (used addAll user in for)");
+        Set<User> resultSecond = sortUser.sortAddUserByAddAll(users);
+        System.out.println();
+        for (User user : resultSecond) {
             System.out.println(user);
         }
 
         System.out.println();
-        System.out.println(users);
-
+        System.out.println(" Result treeSet -> (used addAll user in for)");
+        Set<User> resultThird = sortUser.sortAddUserByConstructor(users);
+        System.out.println();
+        for (User user : resultThird) {
+            System.out.println(user);
+        }
     }
 
 }
