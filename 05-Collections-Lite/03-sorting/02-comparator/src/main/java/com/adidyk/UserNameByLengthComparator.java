b5cf24cca1968ee9513a39a148e71021ed1feae1
androsdav <androsdav@gmail.com>
2017-05-26 00:51:52 +0300

    update

--- 05-Collections-Lite/03-sorting/02-comparator/src/main/java/com/adidyk/UserNameByLengthComparator.java
+++ 05-Collections-Lite/03-sorting/02-comparator/src/main/java/com/adidyk/UserNameByLengthComparator.java
@@ -8,4 +8,5 @@ public class UserNameByLengthComparator implements Comparator<User> {
     public int compare(User o1, User o2) {
         return o1.getName().length() - o2.getName().length();
     }
+
 }
