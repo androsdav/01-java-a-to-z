257cd065fa4efecc4a4858c74d1bc87ca69efbdb
androsdav <androsdav@gmail.com>
2017-06-03 15:06:03 +0300

    update

--- 05-Collections-Pro/01-iterator/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Pro/01-iterator/src/main/java/com/adidyk/StartUi.java
@@ -4,31 +4,7 @@ public class StartUi {
 
     // main - its only for compile jar file
     public static void main(String[] arg) {
-/*
-        System.out.println("IteratorArray");
-        IteratorArray it = new IteratorArray(new int[]{1, 3, 4, 5, 6});
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-        System.out.println("IteratorDoubleArray");
-        IteratorDoubleArray it1 = new IteratorDoubleArray(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-        while (it1.hasNext()) {
-            System.out.println(it1.next());
-        }
-
-        System.out.println("IteratorEvenArray");
-        IteratorEvenArray it2 = new IteratorEvenArray(new int[]{4, 1, 3, 5, 7, 1, 8, 1, 10});
-        while (it2.hasNext()) {
-            System.out.println(it2.next());
-        }
-
-        System.out.println("IteratorPrimeArray");
-        IteratorPrimeArray it3 = new IteratorPrimeArray(new int[]{1, 2, 3, 4, 4, 3, 7, 13, 13, 2, 4, 6, 8, 10, 131});
-        while (it3.hasNext()) {
-            System.out.println(it3.next());
-        }
-*/
+        System.out.println("Iterators");
     }
 
 }
