73ca49612952a6e13455e23c8ceb8aa8776cff68
androsdav <androsdav@gmail.com>
2017-06-09 23:11:44 +0300

    update

--- 05-Collections-Pro/02-generic/src/main/java/com/adidyk/SimpleArray.java
+++ 05-Collections-Pro/02-generic/src/main/java/com/adidyk/SimpleArray.java
@@ -8,12 +8,16 @@ public class SimpleArray<T> {
     private int index = 0;
 
     public SimpleArray() {
-        this.objects = new Object[5];
+        this.objects = new Object[10];
+    }
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
     }
 
     public T add(T object) {
         if (this.index == this.objects.length) {
-            Object[] objectTemp = new Object[(int) round(1.5  * this.objects.length)];
+            Object[] objectTemp = new Object[(int) round(1.5 * this.objects.length)];
             System.arraycopy(this.objects, 0, objectTemp, 0, this.objects.length);
             this.objects = objectTemp;
         }
@@ -21,7 +25,8 @@ public class SimpleArray<T> {
         return object;
     }
 
-    public void removeObjectByObject(T object) {
+    // remove object by object
+    public void remove(T object) {
         for (int index = 0; index < this.objects.length; index++) {
             if (this.objects[index] == object) {
                 System.arraycopy(this.objects, index + 1, this.objects, index, this.objects.length - 1 - index);
@@ -32,10 +37,10 @@ public class SimpleArray<T> {
         Object[] objectTemp = new Object[this.objects.length - 1];
         System.arraycopy(this.objects, 0, objectTemp, 0, this.objects.length - 1);
         this.objects = objectTemp;
-
     }
 
-    public void removeObjectByIndex(int index) {
+    // remove object by index
+    public void remove(int index) {
         if (index < this.objects.length) {
             System.arraycopy(this.objects, index + 1, this.objects, index, this.objects.length - 1 - index);
             this.index--;
@@ -45,11 +50,29 @@ public class SimpleArray<T> {
     }
 
     public Object[] getAllObject() {
-        return this.objects;
+        Object[] objectGet = null;
+        boolean nullTrue = false;
+        for (int index = 0; index < this.objects.length; index++) {
+            if (this.objects[index] == null) {
+                objectGet = new Object[index];
+                System.arraycopy(this.objects, 0, objectGet, 0, index);
+                nullTrue = true;
+                break;
+            }
+        }
+        if (!nullTrue) {objectGet = this.objects;}
+        return objectGet;
     }
 
-
-
-
+    public int size() {
+        int length = 0;
+        for (int index = 0; index < this.objects.length; index++) {
+            if (this.objects[index] == null) {
+                length = index;
+                break;
+            }
+        }
+        return length;
+    }
 
 }
--- 05-Collections-Pro/02-generic/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Pro/02-generic/src/main/java/com/adidyk/StartUi.java
@@ -4,10 +4,11 @@ public class StartUi {
 
     // main - its only for compile jar file
     public static void main(String[] arg) {
+
+        //------------------------------
         System.out.println("SimpleArray");
-
         SimpleArray<String> array = new SimpleArray<>();
-
+        System.out.println("size: " + array.size());
         array.add("1");
         array.add("2");
         array.add("3");
@@ -15,41 +16,69 @@ public class StartUi {
         array.add("5");
         array.add("6");
 
-        System.out.println("Add objects and print all object result:");
+        /*
+        for (int index = 0; index < array.size(); index++) {
+            System.out.println(array.getAllObject());
+        }
+        */
+
+        //------------------------------
+        System.out.println("Add objects and Get all object: ");
         for (Object object : array.getAllObject()) {
             System.out.println(object);
         }
+        System.out.println("size: " + array.size());
 
-        System.out.println("Delete object 4 and print all object result:");
-        array.removeObjectByObject("4");
-
+        //------------------------------
+        System.out.println("Delete object <4> and Get all object:");
+        array.remove("4");
         for (Object object : array.getAllObject()) {
             System.out.println(object);
         }
+        System.out.println("size: " + array.size());
 
-        System.out.println("Add object and print all object result:");
+        //------------------------------
+        System.out.println("Add object <7> and Get all object: ");
         array.add("7");
-
         for (Object object : array.getAllObject()) {
             System.out.println(object);
         }
+        System.out.println("size: " + array.size());
 
-        System.out.println("Delete object by object and print all object result:");
-        array.removeObjectByObject("4");
-
+        //------------------------------
+        System.out.println("Delete object by index <4> and Get all object:");
+        array.remove(4);
         for (Object object : array.getAllObject()) {
             System.out.println(object);
         }
+        System.out.println("size: " + array.size());
 
-        System.out.println("Delete object by index and print all object result:");
-        array.removeObjectByIndex(20);
-        for (Object object : array.getAllObject()) {
-            System.out.println(object);
+/*
+        System.out.println();
+        System.out.println("Its little example for use Collections....  ");
+        System.out.println();
+/*
+        List<String> list = new ArrayList<>(5);
+
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        for (String item : list) {
+            System.out.println(item);
         }
 
+        list.remove("5");
+
+        System.out.println("List size: " +list.size());
 
 
 
+
+
+
+*/
     }
 
 }
