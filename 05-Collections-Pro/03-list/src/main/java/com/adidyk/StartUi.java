74f49e87b65b32c59747d405368a22856688daef
androsdav <androsdav@gmail.com>
2017-06-21 17:45:02 +0300

    update

--- 05-Collections-Pro/03-list/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Pro/03-list/src/main/java/com/adidyk/StartUi.java
@@ -5,5 +5,7 @@ public class StartUi {
     // main - its only for compile jar file
     public static void main(String[] arg) {
         System.out.println("NewArrayList and Comparable");
+        System.out.println("Its only for github");
+
     }
 }
