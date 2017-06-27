f9776b63f9b5221991e27175488db2592c894614
androsdav <androsdav@gmail.com>
2017-06-27 16:51:44 +0300

    update

--- 05-Collections-Pro/03-list/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Pro/03-list/src/main/java/com/adidyk/StartUi.java
@@ -5,5 +5,30 @@ public class StartUi {
     // main - its only for compile jar file
     public static void main(String[] arg) {
         System.out.println("SimpleArrayList and SimpleLinkedList and  Comparable");
+
+        /*
+        Stack<User> stack = new Stack<>();
+        stack.add(new User("1", "Bob", 12));
+        stack.add(new User("2", "Bill", 13));
+        stack.add(new User("3", "Ben", 14));
+        stack.add(new User("4", "Amanda", 15));
+
+        Iterator<User> it = stack.iterator();
+
+        System.out.println("Iterator");
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println("forEach");
+        for (Object user : stack) {
+            System.out.println(user);
+        }
+
+
+        System.out.println(stack);
+        */
+
+
     }
 }
