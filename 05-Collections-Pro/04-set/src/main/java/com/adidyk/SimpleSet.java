832764b2a70df60afb280fdfeec59616cdd0351e
androsdav <androsdav@gmail.com>
2017-07-04 17:45:40 +0300

    update

---
+++ 05-Collections-Pro/04-set/src/main/java/com/adidyk/SimpleSet.java
@@ -0,0 +1,7 @@
+package com.adidyk;
+
+public interface SimpleSet<E> extends Iterable<E> {
+
+    boolean isEmpty();
+    int size();
+}
--- 05-Collections-Pro/04-set/src/main/java/com/adidyk/StartUi.java
+++ 05-Collections-Pro/04-set/src/main/java/com/adidyk/StartUi.java
@@ -1,10 +1,46 @@
 package com.adidyk;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class StartUi {
+
 
     // main - its only for compile jar file
     public static void main(String[] arg) {
-        System.out.println("SimpleArrayList, SimpleLinkedList, SimpleStack, SimpleQueue, Cycle");
+
+
+        Set<User> set = new HashSet<>();
+        System.out.println(set.add(new User("0", "Bill", 0)));
+        set.add(new User("1", "Bob", 1));
+        set.add(new User("2", "Argon", 2));
+        set.add(new User("3", "Fill", 3));
+        set.add(new User("4", "Madly", 4));
+
+/*
+        for(User user :  set) {
+            System.out.println(user + " " + user.hashCode());
+        }
+*/
+        Iterator it = set.iterator();
+        while(it.hasNext()) {
+            User result = (User) it.next();
+            System.out.println(result + " " + result.hashCode());
+        }
+
+        System.out.println();
+
+        Set<String> set1 = new HashSet<>();
+        set1.add("first");
+        set1.add("second");
+        set1.add("third");
+
+        for(String str : set1) {
+            System.out.println(str + " " + str.hashCode());
+        }
+
+
     }
 
 }
