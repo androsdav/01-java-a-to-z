54e0c25fb7c1bc64134cde18f2658b04d424be66
androsdav <androsdav@gmail.com>
2017-10-27 17:55:31 +0300

    update

---
+++ 05-Collections-Pro/07-order-book/src/main/java/com/adidyk/Order.java
@@ -0,0 +1,38 @@
+package com.adidyk;
+
+/**
+ * Class User for create user (object) with params: name, children and birthday.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 21.07.2017.
+ * @version 1.0.
+ */
+public class Order {
+
+    /**
+     * book - is identification action.
+     */
+    private String book;
+
+    /**
+     * operation - is type operation: buy or sell.
+     */
+    private String operation;
+
+    /**
+     * price - is price.
+     */
+    private double price;
+
+    /**
+     * volume - is volume of application, how many lots(shares) to buy / sell.
+     */
+    private int volume;
+
+    /**
+     * id - is id.
+     */
+    private int id;
+
+
+
+}
