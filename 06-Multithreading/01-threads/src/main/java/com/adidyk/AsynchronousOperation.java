08d8331f8f60c4c34fa61157be1be2ca75ccf64d
androsdav <androsdav@gmail.com>
2018-01-03 17:41:57 +0200

    update

--- 06-Multithreading/01-threads/src/main/java/com/adidyk/AsynchronousOperation.java
+++ 06-Multithreading/01-threads/src/main/java/com/adidyk/AsynchronousOperation.java
@@ -3,16 +3,77 @@ package com.adidyk;
 /**
  * Class StartUi for create jar file and run program.
  * @author Didyk Andrey (androsdav@bigmir.net).
- * @since 21.12.2017.
+ * @since 03.01.2017.
  * @version 1.0.
  */
 public class AsynchronousOperation {
+
+    /**
+     * Class Calculate is calculate.
+     */
+    public static final class Calculate implements Runnable {
+
+        /**
+         * @param name - is name thread.
+         */
+        private final String name;
+
+        /**
+         * Calculate - constructor.
+         * @param name is name.
+         */
+        Calculate(final String name) {
+            this.name = name;
+        }
+
+        /**
+         * run - is run.
+         */
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(String.format("%s asynchronous", this.name));
+        }
+    }
+
 
     /**
      * main - is for create jar file and run program.
      * @param arg is nothing.
      */
     public static void main(String[] arg) {
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println("start");
+                new Thread(new Calculate("thread 1")).start();
+                new Thread(new Calculate("thread 2")).start();
+                new Thread() {
+                    @Override
+                    public void run() {
+                        System.out.println("Tread anon asynchronous");
+                    }
+                }.start();
+                System.out.println("finish");
+            }
+        }.start();
+
+
+
+//        System.out.println("start");
+  //      new Thread(new Calculate("thread 1")).start();
+    //    new Thread(new Calculate("thread 2")).start();
+      //  new Thread() {
+        //    @Override
+        //    public void run() {
+        //        System.out.println("Tread anon asynchronous");
+        //    }
+        //}.start();
+        //System.out.println("finish");
         /*
         System.out.println("Thread:");
         Thread thread = new Thread();
