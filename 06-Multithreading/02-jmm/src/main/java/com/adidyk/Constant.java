f6466da2d9fb60945fd6a78e8085cfe86bd7f7cd
androsdav <androsdav@gmail.com>
2018-01-16 17:56:46 +0200

    update

---
+++ 06-Multithreading/02-jmm/src/main/java/com/adidyk/Constant.java
@@ -0,0 +1,36 @@
+package com.adidyk;
+
+/**
+ * Class Constant contains constant.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 09.01.2018.
+ * @version 1.0.
+ */
+class Constant {
+
+    /**
+     * @param SPACE - is sell operation.
+     */
+    static final char SPACE = ' ';
+
+    /**
+     * @param STRING - is string.
+     */
+    static final String STRING = "  a manner  of  speaking that is natural to native  speakers   of  a  language  ";
+
+    /**
+     * @param NAME_THREAD_COUNT_SPACES - is name of thread that do to count number of space in line.
+     */
+    static final String NAME_THREAD_COUNT_SPACES = "space-thread";
+
+    /**
+     * @param NAME_THREAD_COUNT_WORD - is name of thread that do to count number of word in line.
+     */
+    static final String NAME_THREAD_COUNT_WORD = "words-thread";
+
+    /**
+     * @param INFO - is name of thread that do to count number of word in line.
+     */
+    static final String INFO = "[INFO]:";
+
+}
