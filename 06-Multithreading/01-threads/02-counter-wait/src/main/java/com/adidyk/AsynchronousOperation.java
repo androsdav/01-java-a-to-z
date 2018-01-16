7edbc9a0316f6fb5b1113df19eb32a490f9629b7
androsdav <androsdav@gmail.com>
2018-01-16 13:43:29 +0200

    [#1017]

--- 06-Multithreading/01-threads/02-counter-wait/src/main/java/com/adidyk/AsynchronousOperation.java
+++ 06-Multithreading/01-threads/02-counter-wait/src/main/java/com/adidyk/AsynchronousOperation.java
@@ -2,7 +2,6 @@ package com.adidyk;
 
 import static com.adidyk.Constant.NAME_THREAD_COUNT_SPACES;
 import static com.adidyk.Constant.NAME_THREAD_COUNT_WORD;
-import static com.adidyk.Constant.NAME_THREAD_MAIN;
 import static com.adidyk.Constant.STRING;
 import static com.adidyk.Constant.INFO;
 
@@ -19,10 +18,13 @@ public class AsynchronousOperation {
      */
     private void start() {
         System.out.println(String.format("\n%s %s \n", INFO, "PROGRAM -> START"));
-        Thread thread = new Thread(new MainThread(NAME_THREAD_MAIN));
-        thread.start();
+        Thread space = new Thread(new CounterSpace(NAME_THREAD_COUNT_SPACES, STRING));
+        Thread words = new Thread(new CounterWord(NAME_THREAD_COUNT_WORD, STRING));
+        space.start();
+        words.start();
         try {
-            thread.join();
+            space.join();
+            words.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -35,51 +37,6 @@ public class AsynchronousOperation {
      */
     public static void main(String[] arg) {
         new AsynchronousOperation().start();
-    }
-
-    /**
-     * Class MainThread is main thread.
-     * @author Didyk Andrey (androsdav@bigmir.net).
-     * @version 1.0.
-     * @since 09.01.2018.
-     */
-    public static final class MainThread implements Runnable {
-
-        /**
-         * @param name - is name thread.
-         */
-        private final String name;
-
-        /**
-         * MainThread - constructor.
-         * @param name - is name thread.
-         */
-        MainThread(String name) {
-            this.name = name;
-        }
-
-        /**
-         * run - starts two threads: Threads-space and Threads-words.
-         */
-        @Override
-        public void run() {
-            System.out.println(String.format("%s %s %s", INFO, this.name, "-> start"));
-            Thread space = new Thread(new CounterSpace(NAME_THREAD_COUNT_SPACES, STRING));
-            Thread words = new Thread(new CounterWord(NAME_THREAD_COUNT_WORD, STRING));
-            space.start();
-            try {
-                space.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            words.start();
-            try {
-                words.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(String.format("%s %s %s", INFO, this.name, "<- finish"));
-        }
     }
 
 }
