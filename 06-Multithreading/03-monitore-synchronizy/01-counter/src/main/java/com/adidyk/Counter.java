ff8ebf6ede721c0f17488b9bf3daf168f519de19
androsdav <androsdav@gmail.com>
2018-01-22 18:16:26 +0200

    update

--- 06-Multithreading/03-monitore-synchronizy/01-counter/src/main/java/com/adidyk/Counter.java
+++ 06-Multithreading/03-monitore-synchronizy/01-counter/src/main/java/com/adidyk/Counter.java
@@ -1,31 +1,40 @@
 package com.adidyk;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * Class Counter.
  * @author Didyk Andrey (androsdav@bigmir.net).
  * @since 18.01.2018.
  * @version 1.0.
  */
+@ThreadSafe
 class Counter {
 
     /**
      * @param counter - is counter.
      */
-    private long counter = 0L;
+    @GuardedBy("this")
+    private int counter = 0;
 
     /**
      * addCounter - increments counter by one.
      */
-    void addCounter() {
-        this.counter++;
+    synchronized void addCounter() {
+        synchronized (Counter.class) {
+            this.counter++;
+        }
     }
 
     /**
      * getCounter - get counter.
      * @return - get counter.
      */
-    long getCounter() {
-        return this.counter;
+    synchronized int getCounter() {
+        synchronized (Counter.class) {
+            return this.counter;
+        }
     }
 
 }
--- 06-Multithreading/03-monitore-synchronizy/01-counter/src/test/java/com/adidyk/CounterTest.java
+++ 06-Multithreading/03-monitore-synchronizy/01-counter/src/test/java/com/adidyk/CounterTest.java
@@ -1,12 +1,17 @@
 package com.adidyk;
 
-//import static org.junit.Assert.*;
+import org.junit.Test;
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.junit.Assert.assertThat;
 
 /**
- * CounterTest
+ * CounterTest is test counter.
  */
 public class CounterTest {
 
+    /**
+     * ThreadCount is count.
+     */
     private class ThreadCount extends Thread {
 
         /**
@@ -21,6 +26,37 @@ public class CounterTest {
         private ThreadCount(Counter counter) {
             this.counter = counter;
         }
+
+        /**
+         * run - is run.
+         */
+        @Override
+        public void run() {
+            this.counter.addCounter();
+        }
+    }
+
+    /**
+     *
+     * @throws InterruptedException is exception.
+     */
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Counter counter = new Counter();
+        Thread first = new ThreadCount(counter);
+        Thread second = new ThreadCount(counter);
+        Thread third = new ThreadCount(counter);
+        first.start();
+        second.start();
+        third.start();
+        int result = counter.getCounter();
+        System.out.println("Counter: " + counter.getCounter());
+        System.out.println("Counter: " + counter.getCounter());
+        System.out.println("Counter: " + counter.getCounter());
+        System.out.println("Counter: " + result);
+        System.out.println("Counter: " + result);
+        //third.join();
+       // assertThat(counter.getCounter(), is(2));
     }
 
 }
