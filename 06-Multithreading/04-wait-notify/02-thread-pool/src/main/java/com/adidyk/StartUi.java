685e95f0dc80e094bffea46d97fc903f1b70e567
androsdav <androsdav@gmail.com>
2018-03-14 17:50:56 +0200

    update

--- 06-Multithreading/04-wait-notify/02-thread-pool/src/main/java/com/adidyk/StartUi.java
+++ 06-Multithreading/04-wait-notify/02-thread-pool/src/main/java/com/adidyk/StartUi.java
@@ -4,7 +4,7 @@ package com.adidyk;
 
 /** Class StartUi for create jar file and run program (Producer-Consumer).
  * @author Didyk Andrey (androsdav@bigmir.net).
- * @since 03.03.2018.
+ * @since 13.03.2018.
  * @version 1.0.
  */
 public class StartUi {
