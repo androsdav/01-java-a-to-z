eba2d241b8143e46171fab7c402f8eef35bc700e
androsdav <androsdav@gmail.com>
2018-05-24 00:23:12 +0300

    update

---
+++ 06-Multithreading/06-bomberman/01-bomberman/src/main/java/com/adidyk/StartUi.java
@@ -0,0 +1,19 @@
+package com.adidyk;
+
+/**
+ * Class StartUi for create jar file and run program (Parallel Search).
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 23.05.2018.
+ * @version 1.0.
+ */
+public class StartUi {
+
+    /**
+     * main - runs program only for create jar file.
+     * @param arg - is nothing.
+     */
+    public static void main(String[] arg) {
+        System.out.println("BOMBER");
+    }
+
+}
