3f75b1b95092bd755a91f8aef582cb80d6ea9bf9
androsdav <androsdav@gmail.com>
2018-05-29 23:14:32 +0300

    update

--- 06-Multithreading/05-non-blocking-algoritm/src/main/java/com/adidyk/Cache.java
+++ 06-Multithreading/05-non-blocking-algoritm/src/main/java/com/adidyk/Cache.java
@@ -34,6 +34,25 @@ class Cache {
      * @param user - is link variable to object of class User.
      * @return - returns true if user update role, returns false if user not update role..
      */
+    User updateNew(User user) {
+        boolean updateTrue = false;
+        User result = this.cache.computeIfPresent(user.getId(), (key, value) -> {
+            this.cache.get(user.getId()).setRole(user.getRole());
+            return user;
+        });
+        if (result != null) {
+            updateTrue = true;
+        }
+        return result;
+    }
+
+
+
+    /**
+     * update - update user role by key, if container has input key (where key is id user).
+     * @param user - is link variable to object of class User.
+     * @return - returns true if user update role, returns false if user not update role..
+     */
     boolean update(User user) {
         boolean result = false;
         if (this.cache.containsKey(user.getId())) {
--- 06-Multithreading/05-non-blocking-algoritm/src/main/java/com/adidyk/StartUi.java
+++ 06-Multithreading/05-non-blocking-algoritm/src/main/java/com/adidyk/StartUi.java
@@ -14,6 +14,14 @@ public class StartUi {
      */
     public static void main(String[] arg) {
         System.out.println("Non blocking algorithm");
+        Cache cache = new Cache();
+        cache.add(new User(1, "Tom", "operator"));
+        cache.add(new User(2, "Bob", "administrator"));
+        cache.add(new User(3, "Bill", "admin"));
+        User user = cache.updateNew(new User(2, "Amanda", "bitch"));
+        System.out.println(cache.get(2));
+        User user1 = cache.updateNew(new User(4, "Merlin", "demon"));
+        System.out.println(user1);
     }
 
 }
