9359b16781b8ddeb9c016797aed50209cdd8271e
androsdav <androsdav@gmail.com>
2018-08-03 17:37:43 +0300

    update

--- 07-SQL-JDBS/04-jdbc/src/main/java/com/adidyk/Test.java
+++ 07-SQL-JDBS/04-jdbc/src/main/java/com/adidyk/Test.java
@@ -1,9 +1,6 @@
 package com.adidyk;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Class StartUi for create jar file and connect to data base..
@@ -11,7 +8,7 @@ import java.sql.Statement;
  * @since 03.08.2018.
  * @version 1.0.
  */
-public class Test {
+class Test {
 
     /**
      *
@@ -27,8 +24,28 @@ public class Test {
         this.selectOne();
         System.out.println();
         this.selectSecond();
+        System.out.println();
+        this.selectThird();
+        System.out.println();
+        this.insertFirst();
+        System.out.println();
+        this.selectOne();
+        System.out.println();
+        this.insertSecond();
+        System.out.println();
+        this.updateFirst();
+        System.out.println();
+        this.selectOne();
+        System.out.println();
+        this.deleteFirst();
+        System.out.println();
+        this.selectOne();
     }
 
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
     private void selectOne() throws SQLException {
         Statement st = this.connection.createStatement();
         ResultSet rs = st.executeQuery("SELECT * FROM item");
@@ -39,14 +56,92 @@ public class Test {
         st.close();
     }
 
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
     private void selectSecond() throws SQLException {
-        Statement st = this.connection.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM item WHERE id = '1'");
+        PreparedStatement st = this.connection.prepareStatement("SELECT * FROM item WHERE item.id IN (?, ?, ?)");
+        st.setInt(1, 1);
+        st.setInt(2, 3);
+        st.setInt(3, 4);
+        ResultSet rs = st.executeQuery();
         while (rs.next()) {
             System.out.println(String.format("%s  %S", rs.getInt("id"), rs.getString("name")));
         }
         rs.close();
         st.close();
     }
+
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
+    private void selectThird() throws SQLException {
+        PreparedStatement st = this.connection.prepareStatement("SELECT * FROM item WHERE item.id = ?");
+        st.setInt(1, 20);
+        ResultSet rs = st.executeQuery();
+        while (rs.next()) {
+            System.out.println(String.format("%s  %S", rs.getInt("id"), rs.getString("name")));
+        }
+        rs.close();
+        st.close();
+    }
+
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
+    private void insertFirst() throws SQLException {
+        PreparedStatement st = this.connection.prepareStatement("INSERT INTO item(name) VALUES (?)");
+        st.setString(1, "Sell TV test 1 dsdasdasdasdasd");
+        st.executeUpdate();
+        st.close();
+    }
+
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
+    private void insertSecond() throws SQLException {
+        PreparedStatement st = this.connection.prepareStatement("INSERT INTO item(name) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+        st.setString(1, "Insert and get primary key");
+        st.executeUpdate();
+        ResultSet key = st.getGeneratedKeys();
+        if (key.next()) {
+            System.out.println(key.getInt("id"));
+
+        }
+        st.close();
+    }
+
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
+    private void updateFirst() throws SQLException {
+        PreparedStatement st = this.connection.prepareStatement("UPDATE item SET name = ? WHERE id = ?");
+        st.setString(1, "Buy bicycle 3 version");
+        st.setInt(2, 2);
+        st.executeUpdate();
+        st.close();
+    }
+
+    /**
+     *
+     * @throws SQLException - is SQL exception.
+     */
+    private void deleteFirst() throws SQLException {
+        PreparedStatement st = this.connection.prepareStatement("DELETE FROM item WHERE id IN (?, ?)");
+        st.setInt(1, 6);
+        st.setInt(2, 7);
+        st.executeUpdate();
+        st.close();
+    }
+
+
+
+
+
 
 }
