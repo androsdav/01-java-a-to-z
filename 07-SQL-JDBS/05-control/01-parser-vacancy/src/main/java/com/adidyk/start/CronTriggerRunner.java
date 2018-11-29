515866da8190bf4c2b4d63f76aecdd5bbc47797e
androsdav <androsdav@gmail.com>
2018-11-29 12:11:19 +0200

    update

--- 07-SQL-JDBS/05-control/01-parser-vacancy/src/main/java/com/adidyk/start/CronTriggerRunner.java
+++ 07-SQL-JDBS/05-control/01-parser-vacancy/src/main/java/com/adidyk/start/CronTriggerRunner.java
@@ -2,6 +2,10 @@ package com.adidyk.start;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
@@ -18,14 +22,36 @@ public class CronTriggerRunner {
     public void runner() throws SchedulerException {
         //SchedulerFactory factory = new StdSchedulerFactory();
         //Scheduler scheduler = factory.getScheduler();
+        /*
         JobDetail job = newJob(ParserSqlRuJob.class).build();
         Trigger trigger = newTrigger()
                 .withIdentity("CronTrigger")
-                .withSchedule(cronSchedule("0/20 * * * * ?"))
+                .startNow()
+                .withSchedule(cronSchedule("0/30 * * * * ?"))
                 .build();
         SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
         scheduler.start();
         scheduler.scheduleJob(job, trigger);
+        */
+
+        JobDetail job = newJob(ParserSqlRuJob.class).build();
+
+        Set<Trigger> triggers = new HashSet<>();
+        Trigger triggerStartNow = newTrigger()
+                .withIdentity("TriggerStartNow")
+                .startNow()
+                .build();
+        Trigger cronTrigger = newTrigger()
+                .withIdentity("CronTrigger")
+                .startNow()
+                .withSchedule(cronSchedule("0/30 * * * * ?"))
+                .build();
+        triggers.add(triggerStartNow);
+        triggers.add(cronTrigger);
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(job, triggers, false);
     }
 }
