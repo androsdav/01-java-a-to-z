4cc4af300523ea375787ee60f89a971641f56407
androsdav <androsdav@gmail.com>
2018-12-19 22:35:41 +0200

    [#2514]

--- 08-Servlet-JSP/01-servlet/src/main/java/com/adidyk/servlets/GetFormServlet.java
+++ 08-Servlet-JSP/01-servlet/src/main/java/com/adidyk/servlets/GetFormServlet.java
@@ -5,66 +5,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 
-
+/**
+ * Class GetFormServlet uses form and method GET to enter name and surname and returns entered name and surname.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 19.12.2018.
+ * @version 1.0.
+ */
 public class GetFormServlet extends HttpServlet {
 
     /**
-     *
-     * @param request - is req.
-     * @param response - is res.
+     * doGet - uses form and method GET to enter name and surname and returns entered name and surname.
+     * @param request - request.
+     * @param response - response.
      * @throws IOException - io exception.
      */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter writer = new PrintWriter(response.getOutputStream());
-        //String date = String.valueOf(new Date());
         writer.append("<!DOCTYPE html>" +
                 "<html lang=\"en\">" +
-                "<head>" +
-                "<meta charset=\"UTF-8\">" +
-                "<title>01-servlet</title>" +
-                "<style>" +
-                ".table_style {" +
-                "margin: 50px;" +
-                "border: 1px solid #000000;" +
-                "border-collapse: collapse;" +
-                "background: beige;" +
-                "color: black;" +
-                "}" +
-                "TD, TH {" +
-                "padding: 10px;" +
-                "border: 2px solid #000000;" +
-                "text-align: center;" +
-                "}" +
-                "TH {" +
-                "background: darkorange;" +
-                "}" +
-                "</style>" +
-                "</head>" +
-                "<body>" +
-                "<table class =\"table_style\">" +
-                "<tr>" +
-                "<th>#</th>" +
-                "<th>servlets</th>" +
-                "<th>description</th>" +
-                "<th>link</th>"+
-                "<th>response</th>"+
-                "</tr>" +
-                "<tr>" +
-                "<td>1</td>" +
-                "<td>GetFormServlet</td>" +
-                    "<td style=\"width: auto\">Uses form and method GET to enter name and surname and returns entered name and surname</td>" +
-                    "<td><a href=\"index.html\">back</a></td>" +
-                    "<td>" +
-                        "name:" + request.getParameter("name") + "</br>" +
-                        "surname:" + request.getParameter("surname") +
-                    "</td>" +
-                "</tr>" +
-                "</table>" +
-                "</body>" +
+                    "<head>" +
+                        "<link rel=\"stylesheet\" href=\"style.css\">" +
+                        "<meta charset=\"UTF-8\">" +
+                        "<title>01-servlet</title>" +
+                    "</head>" +
+                    "<body>" +
+                        "<table class =\"table_style\">" +
+                            "<tr>" +
+                                "<th>#</th>" +
+                                "<th>servlets</th>" +
+                                "<th>description</th>" +
+                                "<th>link</th>"+
+                                "<th>response</th>"+
+                            "</tr>" +
+                            "<tr>" +
+                                "<td>2</td>" +
+                                "<td>GetFormServlet</td>" +
+                                "<td class=\"td_form_style\">Uses form and method GET to enter name and surname and returns entered name and surname</td>" +
+                                "<td><a href=\"index.html\">back</a></td>" +
+                                "<td>" +
+                                    "name:" + request.getParameter("name") + "</br>" +
+                                    "surname:" + request.getParameter("surname") +
+                                "</td>" +
+                            "</tr>" +
+                        "</table>" +
+                    "</body>" +
                 "</html>"
         );
         writer.flush();
--- 08-Servlet-JSP/01-servlet/src/main/java/com/adidyk/servlets/PostFormServlet.java
+++ 08-Servlet-JSP/01-servlet/src/main/java/com/adidyk/servlets/PostFormServlet.java
@@ -5,18 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
 
 /**
- * Class GetServlet used method doGet for outputs some information about date.
+ * Class PostFormServlet uses form and method POST to enter name and surname and returns entered name and surname.
  * @author Didyk Andrey (androsdav@bigmir.net).
  * @since 19.12.2018.
  * @version 1.0.
  */
-public class GetServlet extends HttpServlet {
+public class PostFormServlet extends HttpServlet {
 
     /**
-     * doGet - outputs some information about date.
+     * doGet - uses form and method POST to enter name and surname and returns entered name and surname.
      * @param request - request.
      * @param response - response.
      * @throws IOException - io exception.
@@ -25,7 +24,6 @@ public class GetServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter writer = new PrintWriter(response.getOutputStream());
-        String date = String.valueOf(new Date());
         writer.append("<!DOCTYPE html>" +
                 "<html lang=\"en\">" +
                     "<head>" +
@@ -43,17 +41,31 @@ public class GetServlet extends HttpServlet {
                                 "<th>response</th>"+
                             "</tr>" +
                             "<tr>" +
-                                "<td>1</td>" +
-                                "<td>GetServlet</td>" +
-                                "<td class=\"td_form_style\">Outputs some information about date</td>" +
+                                "<td>3</td>" +
+                                "<td>GetFormServlet</td>" +
+                                "<td class=\"td_form_style\">Uses form and method POST to enter name and surname and returns entered name and surname</td>" +
                                 "<td><a href=\"index.html\">back</a></td>" +
-                                "<td>" + "date now:" + date + "</td>" +
+                               "<td>" +
+                                    "name:" + request.getParameter("name") + "</br>" +
+                                    "surname:" + request.getParameter("surname") +
+                                "</td>" +
                             "</tr>" +
                         "</table>" +
                     "</body>" +
                 "</html>"
         );
         writer.flush();
+    }
+
+    /**
+     * doPost - returns entered name and surname.
+     * @param request - request.
+     * @param response - response.
+     * @throws IOException - io exception.
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doGet(request, response);
     }
 
 }
