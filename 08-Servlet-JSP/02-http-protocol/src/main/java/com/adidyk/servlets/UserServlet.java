26338740803fc669d7943e6ff10002f188fb54fb
androsdav <androsdav@gmail.com>
2019-01-16 22:49:03 +0200

    update

--- 08-Servlet-JSP/02-http-protocol/src/main/java/com/adidyk/servlets/UserServlet.java
+++ 08-Servlet-JSP/02-http-protocol/src/main/java/com/adidyk/servlets/UserServlet.java
@@ -126,7 +126,7 @@ public class UserServlet extends HttpServlet {
      * doGet - uses form and method POST to enter name and surname and returns entered name and surname.
      * @param request - request.
      * @param response - response.
-     * @throws IOException - io exception. I will do it tomorrow -)))).
+     * @throws IOException - io exception. I will do it tomorrow -)))) Today I do not nothing.
      */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
