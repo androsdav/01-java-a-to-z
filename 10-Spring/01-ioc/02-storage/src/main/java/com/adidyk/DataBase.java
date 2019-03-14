d349b56f22895abe9817d7f3d986e03770307f12
androsdav <androsdav@gmail.com>
2019-03-14 17:54:54 +0200

    update

---
+++ 10-Spring/01-ioc/02-storage/src/main/java/com/adidyk/DataBase.java
@@ -0,0 +1,25 @@
+package com.adidyk;
+
+/**
+ * Interface Storage defines next method for container of users: add.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 09.02.2019.
+ * @version 1.0.
+ */
+public class DataBase {
+
+    /**
+     * DataBase - constructor.
+     */
+    public DataBase() {
+        this.initDataBase();
+    }
+
+    /**
+     * initDataBase
+     */
+    private void initDataBase() {
+        System.out.println("init data base");
+    }
+
+}
---
+++ 10-Spring/01-ioc/02-storage/src/main/java/com/adidyk/Storage.java
@@ -0,0 +1,17 @@
+package com.adidyk;
+
+/**
+ * Interface Storage defines next method for container of users: add.
+ * @author Didyk Andrey (androsdav@bigmir.net).
+ * @since 09.02.2019.
+ * @version 1.0.
+ */
+public interface Storage {
+
+    /**
+     * add - adds user to container.
+     * @param user - user (link variable to object of class User).
+     */
+    User add(User user);
+
+}
