8bfd224fb60dd6b9e5c68e00dba3d9cb204935e0
androsdav <androsdav@gmail.com>
2019-04-02 17:56:26 +0300

    update

---
+++ 10-Spring/01-ioc/02-storage/src/main/resources/create.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE store_temp;
+
+
+SELECT * FROM probe;
+
+INSERT INTO probe(name, login) VALUES('test', 'the best');
+
+
+
+SELECT * FROM probe;
+
+DROP TABLE probe;
+
+
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  login VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO users(login, name) VALUES('goad', 'dilan');
+INSERT INTO users(login, name) VALUES('queen', 'amanda');
+
+
+
+SELECT * FROM users;
