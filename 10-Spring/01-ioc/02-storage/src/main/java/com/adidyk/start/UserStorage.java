7dfedbfe04b1f355fafa90bb45db1f003646161c
androsdav <androsdav@gmail.com>
2019-05-21 18:31:53 +0300

    update

--- 10-Spring/01-ioc/02-storage/src/main/java/com/adidyk/start/UserStorage.java
+++ 10-Spring/01-ioc/02-storage/src/main/java/com/adidyk/start/UserStorage.java
@@ -182,7 +182,7 @@ public class UserStorage {
     }
 
     /**
-     * SearchItemById shows search result - search item by id.
+     * SearchItemById shows search result - search item by id.dfsdfsdf
      * @author Didyk Andrey (androsdav@bigmir.net).
      * @since 06.08.2018.
      * @version 1.0.
