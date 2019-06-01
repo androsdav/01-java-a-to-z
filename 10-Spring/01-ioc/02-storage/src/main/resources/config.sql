5c069adab105fa71f600eb200d1df1618848a323
androsdav <androsdav@gmail.com>
2019-06-01 14:37:08 +0300

    [#1070]

--- 10-Spring/01-ioc/02-storage/src/main/resources/config.sql
+++ 10-Spring/01-ioc/02-storage/src/main/resources/config.sql
@@ -1,26 +1,28 @@
-
+-- create table users
 CREATE TABLE users (
   id serial PRIMARY KEY,
   login VARCHAR(50) NOT NULL,
   name VARCHAR(50) NOT NULL
 );
 
+-- insert user to table users
 INSERT INTO users(login, name) VALUES('goad', 'dilan');
 INSERT INTO users(login, name) VALUES('queen', 'amanda');
 INSERT INTO users(login, name) VALUES('baron', 'bob');
 
-
-
-SELECT * FROM users;
-DROP TABLE users;
-
-
-
+-- insert user
 INSERT INTO users(login, name) VALUES(?, ?)
+-- returns user by id
 SELECT * FROM users WHERE id=?
+-- select user by name
 SELECT * FROM users WHERE name=?
+-- select user by login
 SELECT * FROM users WHERE login=?
+-- select user by name and by login
 SELECT * FROM users WHERE login=? AND name=?
+-- update user by id
 UPDATE users SET login = ?, name = ? WHERE id = ?
+-- delete user by id
 DELETE FROM users WHERE id=?
+-- select all users
 SELECT * FROM users ORDER BY id
